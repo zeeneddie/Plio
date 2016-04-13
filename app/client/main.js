29d05e16211d3c65c5b32e01cbae5d9cbfefc6ea
Michael Rokosh <mike.rokosh@gmail.com>
2016-04-13 15:17:11 +0300

    Added user/organizations null publications

--- app/client/main.js
+++ app/client/main.js
@@ -1,1 +1,7 @@
 import '/imports/startup/client';
+import { Organizations } from '/imports/api/organizations/organizations.js';
+
+Tracker.autorun(function () {
+  console.log(Organizations);
+  // ...
+});
