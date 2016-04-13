7ddbcc2552378720ea7b5f2979849e2bb08e98f1
Denys <etyned@gmail.com>
2016-04-13 18:06:42 +0300

    Setup userpresence package

---
+++ app/imports/startup/server/userpresence.js
@@ -0,0 +1,8 @@
+import { UserPresence, UserPresenceMonitor } from 'meteor/konecty:user-presence';
+
+//SERVER
+// Listen for new connections, login, logoff and application exit to manage user status and register methods to be used by client to set user status and default status
+UserPresence.start();
+// Active logs for every changes
+// Listen for changes in UserSessions and Meteor.users to set user status based on active connections
+UserPresenceMonitor.start();
