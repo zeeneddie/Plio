f7b7f622a32878e252d5ee1b20bfa462c30ef8d8
RevelsGit <revels0k@yandex.ru>
2016-04-13 16:41:27 +0300

    organization dashboard page routing

--- app/imports/startup/client/routes.js
+++ app/imports/startup/client/routes.js
@@ -3,12 +3,22 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 import '/imports/ui/layouts';
 import '/imports/ui/components';
+import '/imports/ui/pages';
 
 
 FlowRouter.route('/', {
   name: 'dashboard',
   action: function(params) {
     BlazeLayout.render('DashboardLayout');
+  }
+});
+
+FlowRouter.route('/:organizationId', {
+  name: 'dashboard',
+  action: function(params) {
+    BlazeLayout.render('DashboardLayout', {
+      content: 'DashboardPage'
+    });
   }
 });
 
