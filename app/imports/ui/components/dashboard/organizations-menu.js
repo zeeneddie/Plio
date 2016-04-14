56cf5dadf852b9a8a2ebbcda970041f92b465b84
Denys <etyned@gmail.com>
2016-04-14 13:51:01 +0300

    Implement userpresence for user dropdown

--- app/imports/ui/components/dashboard/organizations-menu.js
+++ app/imports/ui/components/dashboard/organizations-menu.js
@@ -2,7 +2,7 @@ import { Organizations } from '/imports/api/organizations/organizations.js';
 
 Template.OrganizationsMenu.helpers({
   organizations() {
-    return Organizations.find({ 'users.userId': Meteor.userId() });
+    return Organizations.find({'users.userId': Meteor.userId()});
   },
   regex() {
     return `^\\/organizations/${this._id}`;
--- app/imports/ui/components/dashboard/user-menu.js
+++ app/imports/ui/components/dashboard/user-menu.js
@@ -1,9 +1,60 @@
+import { UserPresence } from 'meteor/konecty:user-presence';
+
+const STATUSES = [
+  {
+    text: 'Online',
+    css: 'text-success',
+    status: 'online'
+  },
+  {
+    text: 'Away',
+    css: 'text-warning',
+    status: 'away'
+  },
+  {
+    text: 'Offline',
+    css: 'text-danger',
+    status: 'offline'
+  }
+];
+
+Template.UserMenu.helpers({
+  getStatuses() {
+    return STATUSES;
+  },
+  isActiveStatus(index) {
+    const user = Meteor.user();
+    const currentStatus = STATUSES[index].text.toLowerCase();
+    const statusDefault = user.statusDefault;
+
+    return currentStatus === statusDefault;
+  },
+  getActiveClass() {
+    const user = Meteor.user();
+    const userStatus = user.status;
+    const activeStatus = STATUSES.find((status) => {
+      return status.text.toLowerCase() === userStatus;
+    });
+
+    return activeStatus.css;
+  }
+});
+
 Template.UserMenu.events({
-  'click a.logout-link': function(e, tpl) {
+  'click a.logout-link': function (e, tpl) {
     e.preventDefault();
 
     Meteor.logout(() => {
       FlowRouter.go('signIn');
     });
+  },
+
+  'click a.js-status': function (e, tpl) {
+    e.preventDefault();
+
+    const status = $(e.target).text().trim().toLowerCase();
+    if (status != Meteor.user().statusDefault) {
+      Meteor.call('UserPresence:setDefaultStatus', status);
+    }
   }
 });
