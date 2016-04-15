e47c5a36652e6a2157c32e952900c436561d39f7
RevelsGit <revels0k@yandex.ru>
2016-04-15 16:20:53 +0300

    replaced the organization logo with text label

--- app/imports/ui/components/dashboard/organizations-menu/organizations-menu.js
+++ app/imports/ui/components/dashboard/organizations-menu/organizations-menu.js
@@ -5,6 +5,10 @@ import { Blaze } from 'meteor/blaze';
 import { Organizations } from '/imports/api/organizations/organizations.js';
 
 Template.OrganizationsMenu.helpers({
+  organization() {
+    const _id = FlowRouter.getParam("_id");
+    return Organizations.findOne(_id);
+  },
   organizations() {
     return Organizations.find({ 'users.userId': Meteor.userId() });
   },
