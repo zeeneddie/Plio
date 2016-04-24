cb4c4814a7ab8317c6a6f24748460f4bd9fd96e5
meyginix <meyginix@gmail.com>
2016-04-24 22:41:00 +0300

    ui for standards books sections

--- app/imports/ui/components/dashboard/organization-settings/standards-book-section.js
+++ app/imports/ui/components/dashboard/organization-settings/standards-book-section.js
@@ -1,22 +1,16 @@
-import {
-  StandardsBookSections
-} from '/imports/api/standards-book-sections/standards-book-sections.js';
-import {
-  insert, update, remove
-} from '/imports/api/standards-book-sections/methods.js';
-
-
 Template.OrganizationSettings_StandardsBookSection.viewmodel({
+  name: '',
+  number: '',
   title: '',
-  onCreated() {
+  autorun() {
     if (this._id) {
       this.title(`${this.number()}. ${this.name()}`);
     }
   },
-  shouldSave() {
-    const tplData = this.templateInstance.data;
-    const storedName = tplData.name;
-    const storedNum = tplData.number;
+  isChanged() {
+    const context = this.templateInstance.data;
+    const storedName = context.name;
+    const storedNum = context.number;
 
     const { name, number } = this.getData();
 
@@ -25,63 +19,33 @@ Template.OrganizationSettings_StandardsBookSection.viewmodel({
       (name !== storedName) || (number !== storedNum)
     ]);
   },
-  save() {
-    if (!this.shouldSave()) return;
+  onFocusOut() {
+    this.updateData();
 
-    const { name, number } = this.getData();
-
-    if (!this._id) {
-      const organizationId = this.organizationId();
-
-      insert.call({ name, number, organizationId }, (err, res) => {
-        if (err) {
-          console.log(err);
-          toastr.error('Failed to create a standards book section');
-        }
-
-        this.destroy();
-      });
-    } else {
-      const _id = this._id();
-
-      update.call({ _id, name, number }, (err, res) => {
-        if (err) {
-          toastr.error('Failed to update a standards book section');
-        }
-      });
+    if (this.isChanged()) {
+      this.onChange(this);
     }
   },
   delete() {
-    if (!this._id) {
-      this.destroy();
-      return;
-    }
-
-    if (!confirm('Delete this standards book section?')) return;
-
-    const _id = this._id();
-
-    remove.call({ _id }, (err, res) => {
-      if (err) {
-        toastr.error('Failed to remove a standards book section');
-      }
-    });
+    this.onDelete(this);
   },
-  getData() {
-    let name, number;
-
+  updateData() {
     const title = this.title();
-    const regex = /^([0-9]+)(?:\.|\s)\s*(\S+)$/;
-
+    const regex = /^([0-9]+)(?:\.|\s)\s*([a-z].+)$/i;
     const match = title.match(regex);
+    
     if (match) {
       number = Number(match[1]);
       name = match[2];
-    }
 
-    return { name, number };
+      this.name(name);
+      this.number(number);
+    }
   },
-  destroy() {
-    Blaze.remove(this.templateInstance.view);
+  getData() {
+    return { 
+      name: this.name(), 
+      number: Number(this.number()) 
+    };
   }
 });
