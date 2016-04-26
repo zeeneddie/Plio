0a8f765688fc8dfe34c645b53767b2df69d2bb1c
RevelsGit <revels0k@yandex.ru>
2016-04-26 14:45:23 +0300

    fixed damn clear field bug

--- app/imports/ui/components/standards/includes/edit-standard/edit-standard.js
+++ app/imports/ui/components/standards/includes/edit-standard/edit-standard.js
@@ -3,12 +3,13 @@ import { Template } from 'meteor/templating';
 import { StandardsTypes } from '/imports/api/standards-types/standards-types.js';
 
 Template.EditStandard.viewmodel({
-  titleValue: '',
+  titleText: '',
+  isFocused: false,
   description: '',
   type: '',
   types() {
     return [{ _id: '', name: '' }].concat(StandardsTypes.find({}).fetch()); // add empty option
   },
   issueNumber: 1,
-  status: 'draft',
+  status: 'draft'
 });
