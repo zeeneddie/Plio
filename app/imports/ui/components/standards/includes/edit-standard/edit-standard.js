b2494766a1ac339ce28129b5a3395071d130453f
RevelsGit <revels0k@yandex.ru>
2016-04-26 13:18:37 +0300

    status field

--- app/imports/ui/components/standards/includes/edit-standard/edit-standard.js
+++ app/imports/ui/components/standards/includes/edit-standard/edit-standard.js
@@ -3,11 +3,15 @@ import { Template } from 'meteor/templating';
 import { StandardsTypes } from '/imports/api/standards-types/standards-types.js';
 
 Template.EditStandard.viewmodel({
+  autorun() {
+    console.log(this.status());
+  },
   titleValue: '',
   description: '',
-  selectedType: '',
+  type: '',
   types() {
     return [{ _id: '', name: '' }].concat(StandardsTypes.find({}).fetch()); // add empty option
   },
   issueNumber: 1,
+  status: 'draft',
 });
