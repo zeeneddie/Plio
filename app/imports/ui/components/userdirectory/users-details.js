4d1b81d16f2d307303095debd6e5c8f38c6a1feb
Denys <etyned@gmail.com>
2016-04-27 12:23:47 +0300

    Update fixtures, update users details page

--- app/imports/ui/components/userdirectory/users-details.js
+++ app/imports/ui/components/userdirectory/users-details.js
@@ -1,7 +1,34 @@
 import { Template } from 'meteor/templating';
 
+import {Organizations} from '/imports/api/organizations/organizations.js';
+
 Template.UsersDetails.viewmodel({
   activeUser() {
-    return this.parent().activeUser();
+    const userId = this.parent().activeUser();
+    return Meteor.users.findOne({ _id: userId });
+  },
+  initials(user) {
+    const { firstName='', lastName='' } = user.profile;
+    let initials = '';
+    if (firstName) {
+      initials += firstName.charAt(0);
+    }
+    
+    if (lastName) {
+      initials += lastName.charAt(0);
+    }
+    
+    return initials.toUpperCase(); 
+  },
+  email(user) {
+    return user.emails[0].address;
+  },
+  address(user) {
+    const { address='' } = user.profile;
+    return address;
+  },
+  organization() {
+    const serialNumber = Number(FlowRouter.getParam('orgSerialNumber'));
+    return Organizations.findOne({ serialNumber });
   }
 });
