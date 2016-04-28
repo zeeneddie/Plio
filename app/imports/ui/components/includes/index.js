d3de4ddc2714d306ac2258187af8ce1b248330a4
Taras Dolynyuk <trsdln@gmail.com>
2016-04-28 16:11:37 +0300

    Move modal window into modals

--- app/imports/ui/components/includes/index.js
+++ app/imports/ui/components/includes/index.js
@@ -1,5 +1,3 @@
-import './modal-window.html';
-import './modal-window.js';
 import './simple-card.html';
 import './time-picker.html';
 import './time-picker.js';
--- app/imports/ui/components/modals/index.js
+++ app/imports/ui/components/modals/index.js
@@ -1,3 +1,5 @@
 import './editable-modal.html';
 import './editable-modal.js';
+import './modal-window.html';
+import './modal-window.js';
 import './modal-wrapper.html';
