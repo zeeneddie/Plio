8ee1000f0c1cb71c55eed8d20697232f949b313e
Praxie <mr.praxie@gmail.com>
2016-05-03 16:29:23 +0300

    t95 user routes, my profile

--- app/imports/ui/pages/user-directory-page.js
+++ app/imports/ui/pages/user-directory-page.js
@@ -5,40 +5,50 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Organizations } from '/imports/api/organizations/organizations.js';
 
 Template.UserDirectoryPage.viewmodel({
-  activeUser: null,
+  activeUser() {
+    return FlowRouter.getParam('userId') || null;
+  },
+
+  getCurrentOrganizationSerialNumber() {
+    return parseInt(FlowRouter.getParam('orgSerialNumber'));
+  }, 
+
   autorun() {
     const organizationsHandle = this.templateInstance.subscribe('currentUserOrganizations');
     
     if (organizationsHandle.ready()) {
-      const userIds = getOrganizationUsers();
-      if (userIds) {
-        this.templateInstance.subscribe('organizationUsers', userIds);
-      } else {
-        FlowRouter.go('signIn');
+      const userIds = this.getCurrentOrganizationUsers();
+      if (userIds && userIds.length) {
+        const organizationUsersHandle = this.templateInstance.subscribe('organizationUsers', userIds);
+        if (!this.activeUser() && organizationUsersHandle.ready()) {
+          FlowRouter.go('userDirectoryUserPage', { 
+            orgSerialNumber: this.getCurrentOrganizationSerialNumber(), 
+            userId: this.organizationUsers().fetch()[0]._id
+          });
+        }
       }
     }
   },
+
   user() {
     return Meteor.users.findOne({ _id: this.activeUser() })
   },
+
   organizationUsers() {
-    const userIds = getOrganizationUsers();
+    const userIds = this.getCurrentOrganizationUsers();
 
     if (userIds) {
       this.activeUser(userIds[0]);
 
-      return Meteor.users.find({ _id: { $in: userIds }}, { sort: { 'profile.firstName': 1 }});
+      return Meteor.users.find({ _id: { $in: userIds }}, { sort: { 'profile.firstName': 1, 'emails.0.address': 1 }});
+    }
+  },
+
+  getCurrentOrganizationUsers() {
+    const organization = Organizations.findOne({ serialNumber: this.getCurrentOrganizationSerialNumber() });
+    if (organization) {
+      const { users } = organization;
+      return _.pluck(users, 'userId');
     }
   }
 });
-
-function getOrganizationUsers() {
-  const serialNumber = Number(FlowRouter.getParam('orgSerialNumber'));
-  const organization  = Organizations.findOne({ serialNumber });
-  if (organization) {
-    const { users } = organization;
-    return _.pluck(users, 'userId');
-  }
-
-  return null;
-}
