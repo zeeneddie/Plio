5078e4994bdd29e6aafcb882593c93961d7e7f62
RevelsGit <revels0k@yandex.ru>
2016-05-04 17:43:34 +0300

    update standard owner

--- app/imports/ui/components/standards/includes/fields/owner/owner.js
+++ app/imports/ui/components/standards/includes/fields/owner/owner.js
@@ -1,10 +1,13 @@
 import { Template } from 'meteor/templating';
 
 Template.ESOwner.viewmodel({
-  mixin: ['search', 'user'],
-  onRendered() {
-    if (this.hasUser()) {
+  mixin: ['search', 'user', 'modal'],
+  onCreated() {
+    if (this.hasUser() && !this._id) {
       Meteor.setTimeout(() => this.selectOwner(Meteor.user()), 0);
+    } else if (this.hasUser() && this._id) {
+      const fullName = this.userFullNameOrEmail(this.selectedOwnerId());
+      Meteor.setTimeout(() => this.owner(fullName), 0);
     }
   },
   owner: '',
@@ -19,6 +22,16 @@ Template.ESOwner.viewmodel({
     const fullName = this.userFullNameOrEmail(doc);
     this.owner(fullName);
     this.selectedOwnerId(_id);
+    this.update();
+  },
+  update() {
+    if (!this._id) return;
+    const { owner } = this.getData();
+    if (!owner) {
+      this.modal().error('Owner is required!');
+    }
+
+    this.parent().update({ owner });
   },
   getData() {
     const { selectedOwnerId:owner } = this.data();
