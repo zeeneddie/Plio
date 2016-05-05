e833ee14acaf55a50fd0e7c651d68e9ad6caaeab
RevelsGit <revels0k@yandex.ru>
2016-05-05 10:14:14 +0300

    update standard issue number and status:

--- app/imports/ui/components/standards/includes/fields/issue-number/issue-number.js
+++ app/imports/ui/components/standards/includes/fields/issue-number/issue-number.js
@@ -1,7 +1,16 @@
 import { Template } from 'meteor/templating';
 
 Template.ESIssueNumber.viewmodel({
+  mixin: 'modal',
   issueNumber: 1,
+  update() {
+    if (!this._id) return;
+    const { issueNumber } = this.getData();
+    if (!issueNumber) {
+      this.modal().error('Issue number is required!');
+    }
+    this.parent().update({ issueNumber });
+  },
   getData() {
     const { issueNumber } = this.data();
     return { issueNumber: parseInt(issueNumber, 10) };
--- app/imports/ui/components/standards/includes/fields/status/status.js
+++ app/imports/ui/components/standards/includes/fields/status/status.js
@@ -2,6 +2,19 @@ import { Template } from 'meteor/templating';
 
 Template.ESStatus.viewmodel({
   status: 'issued',
+  mixin: 'modal',
+  changeStatus(status) {
+    this.status(status);
+    this.update();
+  },
+  update() {
+    if (!this._id) return;
+    const { status } = this.getData();
+    if (!status) {
+      this.modal().error('Status is required!');
+    }
+    this.parent().update({ status });
+  },
   getData() {
     const { status } = this.data();
     return { status };
