cb12ea77b3b139b95986589123f58e7a89fc03ae
meyginix <meyginix@gmail.com>
2016-05-10 15:35:17 +0300

    removed redudant code

--- app/imports/ui/components/userdirectory/user-edit/password.js
+++ app/imports/ui/components/userdirectory/user-edit/password.js
@@ -1,18 +1,3 @@
 Template.UserEdit_Password.viewmodel({
-  mixin: 'collapse',
-  oldPassword: '',
-  newPassword: '',
-  confirmPassword: '',
-  isEditable() {
-    return this.parent().isEditable();
-  },
-  changePassword() {
-    this.parent().changePassword(this);
-  },
-  getData() {
-    return {
-      oldPassword: this.oldPassword(),
-      newPassword: this.newPassword()
-    };
-  }
+  mixin: 'collapse'
 });
