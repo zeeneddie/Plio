593cf11f1ff50828bc8957ba10da1a8c6f334379
Denys <etyned@gmail.com>
2016-05-11 18:41:42 +0300

    Add superpowers to user directory

--- app/imports/ui/components/userdirectory/users-details.js
+++ app/imports/ui/components/userdirectory/users-details.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { UserRolesNames } from '../../../api/constants.js';
 
 Template.UsersDetails.viewmodel({
   mixin: ['user', 'organization', 'modal'],
@@ -6,10 +7,25 @@ Template.UsersDetails.viewmodel({
     return `${type} phone`;
   },
   superpowersTitle(user) {
-    if(this.organization()) {
+    if (this.organization()) {
       return `${this.userFullNameOrEmail(user)}'s superpowers for ${this.organization().name}`
     }
   },
+  superpowers(user) {
+    if (this.organization()) {
+      const orgId = this.organization()._id;
+      const userRoles = user.roles[orgId];
+
+      const superpowers = Object.keys(UserRolesNames).map((key) => {
+        return { key: key, value: UserRolesNames[key], flag: userRoles.indexOf(key) !== -1 };
+      });
+      
+      return superpowers.sort((a, b) => {
+        return b.flag - a.flag;
+      });
+    }
+  },
+
   openEditUserModal(e) {
     e.preventDefault();
     this.modal().open({
