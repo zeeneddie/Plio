bdd4e2f96e8e4a09310fa5c4290997842a6af2f2
Denys <etyned@gmail.com>
2016-05-12 14:31:03 +0300

    Add search for nested standards

--- app/imports/ui/components/standards/standards-list.js
+++ app/imports/ui/components/standards/standards-list.js
@@ -1,14 +1,38 @@
 import { Template } from 'meteor/templating';
 
 import { StandardsBookSections } from '/imports/api/standards-book-sections/standards-book-sections.js';
+import { Standards } from '/imports/api/standards/standards.js'
 
 Template.StandardsList.viewmodel({
   share: 'search',
   mixin: ['modal', 'search'],
-  stadardsBookSections() {
-    const query = this.searchObject('searchText', 'title');
+  standardsBookSections() {
+    const searchQuery = this.searchObject('searchText', 'title');
+
+    const availableSections = StandardsBookSections.find({}).fetch();
+    const sectionIds = _.pluck(availableSections, '_id');
+    
+    const standardsQuery = { 
+      $and: [
+        { sectionId: { $in: sectionIds } },
+        searchQuery
+      ]
+    };
+    const standards = Standards.find(standardsQuery).fetch();
+    
+    const filteredSectionIds = sectionIds.filter((id) => {
+      return _.some(standards, (s) => s.sectionId === id);
+    });
+    const sectionsQuery = {
+      $or: [
+        { _id: { $in: filteredSectionIds } },
+        searchQuery
+      ]
+    };
+    
     const options = { sort: { title: 1 } };
-    return StandardsBookSections.find(query, options);
+    
+    return StandardsBookSections.find(sectionsQuery, options);
   },
   openAddTypeModal(e) {
     this.modal().open({
