0bdd79c13d0b46a3b214ced95408b1422579800f
Michael Rokosh <mike.rokosh@gmail.com>
2016-05-12 20:11:50 +0300

    Fixed redirect issue in ud

--- app/imports/api/users/server/publications.js
+++ app/imports/api/users/server/publications.js
@@ -4,23 +4,6 @@ Meteor.publish(null, function() {
   return Meteor.users.find({ _id: this.userId });
 });
 
-Meteor.publish('organizationUsers', function (orgId) {
-  const organization = Organizations.findOne({ 
-    _id: orgId, 
-    users: { 
-      $elemMatch: { 
-        userId: this.userId, 
-        isDeleted: { $ne: false } 
-      } 
-    } 
-  });
-  if (!organization) {
-    throw new Meteor.Error('organization-not-found', 'Organization Not Found');
-  }
-  const { users } = organization;
-  const existingUsersIds = _.filter(users, (usrDoc) => {
-    const { isRemoved, removedBy, removedAt } = usrDoc;
-    return !isRemoved && !removedBy && !removedAt;
-  });
-  return Meteor.users.find({ _id: { $in: _.pluck(existingUsersIds, 'userId') } });
+Meteor.publish('organizationUsers', function (userIds) {
+  return Meteor.users.find({_id: {$in: userIds}});
 });
