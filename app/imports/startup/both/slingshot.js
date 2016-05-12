58b2fdb0e3485e06c59ad4acdba487965a336193
meyginix <meyginix@gmail.com>
2016-05-12 15:54:47 +0300

    slingshot configuration for standards attachments

--- app/imports/startup/both/slingshot.js
+++ app/imports/startup/both/slingshot.js
@@ -5,3 +5,8 @@ Slingshot.fileRestrictions('usersAvatars', {
   allowedFileTypes: ['image/png', 'image/jpeg', 'image/gif'],
   maxSize: 10 * 1024 * 1024 // 10 MB
 });
+
+Slingshot.fileRestrictions('standardsAttachments', {
+  allowedFileTypes: /[a-z]+\/[a-z0-9\-]+/i,
+  maxSize: 64 * 1024 * 1024 // 64 MB
+});
