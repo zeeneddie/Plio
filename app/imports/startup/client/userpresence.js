cc7878287e278a6fe9625662f116fb302e9aefc3
Denys <etyned@gmail.com>
2016-05-17 16:15:40 +0300

    Refactor user presence

--- app/imports/startup/client/userpresence.js
+++ app/imports/startup/client/userpresence.js
@@ -2,6 +2,6 @@ import { UserPresence } from 'meteor/konecty:user-presence';
 
 UserPresence.awayTime = 300000;
 // Set user as away when window loses focus. Defaults false
-UserPresence.awayOnWindowBlur = true;
+UserPresence.awayOnWindowBlur = false;
 // Start monitor for user activity
 UserPresence.start();
