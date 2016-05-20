d42eb56e3f32f2a0b0429c043ab03ae2ee8cbc98
RevelsGit <revels0k@yandex.ru>
2016-05-20 10:46:51 +0300

    ...

--- app/imports/ui/pages/user-directory-page.js
+++ app/imports/ui/pages/user-directory-page.js
@@ -10,9 +10,6 @@ Template.UserDirectoryPage.viewmodel({
   onCreated() {
     this.searchText('');
   },
-  activeUser() {
-    return FlowRouter.getParam('userId') || null;
-  },
   autorun() {
     const userIds = this.getCurrentOrganizationUsers();
     if (userIds && userIds.length) {
@@ -28,7 +25,9 @@ Template.UserDirectoryPage.viewmodel({
   currentUser() {
     return this.activeUser() && Meteor.users.findOne({ _id: this.activeUser() });
   },
-
+  activeUser() {
+    return FlowRouter.getParam('userId') || null;
+  },
   organizationUsers() {
     const userIds = this.getCurrentOrganizationUsers();
     const findQuery = {};
