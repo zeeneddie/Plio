f924803254e043f400dc351fc5b3db27845f2b9c
meyginix <meyginix@gmail.com>
2016-05-25 12:39:50 +0300

    fixed hello page

--- app/imports/ui/pages/hello-page.js
+++ app/imports/ui/pages/hello-page.js
@@ -5,28 +5,30 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Organizations } from '/imports/api/organizations/organizations.js';
 
 Template.HelloPage.viewmodel({
-  autorun() {
-    const currentUser = Meteor.user();
-    const organizationsHandle = this.templateInstance.subscribe('currentUserOrganizations');
-    if (!Meteor.loggingIn() && organizationsHandle.ready()) {
-      if (currentUser) {
-        // if the user is an owner of organization go to that organization no matter what
-        const ownerOrg = Organizations.findOne({ 'users': { $elemMatch: { userId: currentUser._id, role: 'owner' } } });
-        if (ownerOrg) {
-          this.routeToDashboard(ownerOrg);
-        } else {
-          const { selectedOrganizationSerialNumber } = currentUser;
-          if (selectedOrganizationSerialNumber) {
-            FlowRouter.go('dashboardPage', { orgSerialNumber: selectedOrganizationSerialNumber });
+  onCreated() {
+    this.templateInstance.autorun(() => {
+      const currentUser = Meteor.user();
+      const organizationsHandle = this.templateInstance.subscribe('currentUserOrganizations');
+      if (!Meteor.loggingIn() && organizationsHandle.ready()) {
+        if (currentUser) {
+          // if the user is an owner of organization go to that organization no matter what
+          const ownerOrg = Organizations.findOne({ 'users': { $elemMatch: { userId: currentUser._id, role: 'owner' } } });
+          if (ownerOrg) {
+            this.routeToDashboard(ownerOrg);
           } else {
-            const org = Organizations.findOne({ 'users.userId': currentUser._id });
-            this.routeToDashboard(org);
+            const { selectedOrganizationSerialNumber } = currentUser;
+            if (selectedOrganizationSerialNumber) {
+              FlowRouter.go('dashboardPage', { orgSerialNumber: selectedOrganizationSerialNumber });
+            } else {
+              const org = Organizations.findOne({ 'users.userId': currentUser._id });
+              this.routeToDashboard(org);
+            }
           }
+        } else {
+          FlowRouter.go('signIn');
         }
-      } else {
-        FlowRouter.go('signIn');
       }
-    }
+    });
   },
   routeToDashboard(doc) {
     const { serialNumber } = doc;
