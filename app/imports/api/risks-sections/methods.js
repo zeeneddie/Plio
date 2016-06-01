59fcf99a671728a31cb498adc71812c3f9daceec
RevelsGit <revels0k@yandex.ru>
2016-06-01 13:03:04 +0300

    fixed basic method

--- app/imports/api/risks-sections/methods.js
+++ app/imports/api/risks-sections/methods.js
@@ -6,9 +6,9 @@ import { RisksSectionSchema } from './risks-section-schema.js';
 import { RisksSections } from './risks-sections.js';
 
 export const insert = new ValidatedMethod({
-  name: 'Risks.insert',
+  name: 'RisksSections.insert',
 
-  validate: RisksSchema.validator(),
+  validate: RisksSectionSchema.validator(),
 
   run({ ...args }) {
     const userId = this.userId;
@@ -18,6 +18,6 @@ export const insert = new ValidatedMethod({
       );
     }
 
-    return RisksService.insert({ ...args });
+    return RisksSectionService.insert({ ...args });
   }
 });
