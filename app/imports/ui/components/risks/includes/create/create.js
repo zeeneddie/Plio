3dfb7800f6ce15114eab59a508e47d7e96af29fc
RevelsGit <revels0k@yandex.ru>
2016-06-02 13:16:04 +0300

    creating new risks

--- app/imports/ui/components/risks/includes/create/create.js
+++ app/imports/ui/components/risks/includes/create/create.js
@@ -1,7 +1,47 @@
 import { Template } from 'meteor/templating';
 
+import { insert } from '/imports/api/risks/methods.js';
+
 Template.CreateRisk.viewmodel({
+  mixin: ['modal', 'organization'],
   save() {
-    
+    const data = this.getData();
+
+    for (let key in data) {
+      if (!data[key]) {
+        const errorMessage = `The new standard cannot be created without a ${key}. Please enter a ${key} for your standard.`;
+        this.modal().setError(errorMessage);
+        return;
+      }
+    }
+
+    this.insert(data);
+  },
+  insert({ title, typeId, owner }) {
+    const organizationId = this.organizationId();
+    const owners = [owner];
+
+    const args = {
+      title,
+      typeId,
+      owners,
+      organizationId
+    };
+
+    this.modal().callMethod(insert, args, (err, _id) => {
+      if (err) {
+        return;
+      }
+
+      this.modal().close();
+    });
+  },
+  getData() {
+    let data = {};
+
+    this.children(vm => vm.getData && vm.getData())
+        .forEach(vm => data = { ...data, ...vm.getData() });
+
+    return data;
   }
 });
