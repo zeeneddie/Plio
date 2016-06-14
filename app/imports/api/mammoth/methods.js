2d748c71ba04c5b5c3b13c726740457df9e41f6f
nazclarion <nazclarion@gmail.com>
2016-06-15 01:33:31 +0800

    Async docx rendering

--- app/imports/api/mammoth/methods.js
+++ app/imports/api/mammoth/methods.js
@@ -20,6 +20,7 @@ export const convertDocxToHtml = new ValidatedMethod({
         }
     }).validator(),
     run({ url, options }) {
+        this.unblock();
         const fut = new Future();
 
         HTTP.call('GET', url, {
