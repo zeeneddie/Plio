4926ba2046beaf337f42504e9fb3e667f9bf94b7
RevelsGit <revels0k@yandex.ru>
2016-06-17 17:56:45 +0300

    file-uploader-wrapper component, other files subcard

--- app/imports/api/non-conformities/non-conformities-schema.js
+++ app/imports/api/non-conformities/non-conformities-schema.js
@@ -126,6 +126,30 @@ const OptionalSchema = new SimpleSchema([
         }
       }
     },
+    'files': {
+      type: [Object],
+      optional: true
+    },
+    'files.$._id': {
+      type: String,
+      regEx: SimpleSchema.RegEx.Id
+    },
+    'files.$.extension': {
+      type: String,
+      autoValue() {
+        if (this.isSet) {
+          return this.value.toLowerCase();
+        }
+      },
+    },
+    'files.$.url': {
+      type: String,
+      regEx: SimpleSchema.RegEx.Url,
+      optional: true
+    },
+    'files.$.name': {
+      type: String
+    },
     ...rootCauseAnalysis,
     ...updateOfStandards
   }
---
+++ app/imports/ui/components/includes/file-uploader-wrapper/file-uploader-wrapper.js
@@ -0,0 +1,88 @@
+import { Template } from 'meteor/templating';
+
+Template.FileUploader_Wrapper.viewmodel({
+  mixin: 'filesList',
+  files: [],
+  slingshotDirective: '',
+  insertFileFn() {
+    return this.insertFile.bind(this);
+  },
+  insertFile({ _id, name }, cb) {
+    const fileDoc = { _id, name, extension: name.split('.').pop().toLowerCase() };
+
+    if (this.files() && this.files().length) {
+
+      const options = {
+        $push: {
+          files: fileDoc
+        }
+      };
+
+      this.parent().update({ options }, cb);
+    } else {
+      this.parent().update({
+        files: [fileDoc]
+      }, cb);
+    }
+  },
+  onUploadCb() {
+    return this.onUpload.bind(this);
+  },
+  onUpload(err, { _id, url }) {
+    if (err && err.error !== 'Aborted') {
+      ViewModel.findOne('ModalWindow').setError(err.reason);
+      return;
+    }
+
+    const query = {
+      files: {
+        $elemMatch: { _id }
+      }
+    };
+    const options = {
+      $set: {
+        'files.$.url': url
+      }
+    };
+
+    this.parent().update({ query, options });
+  },
+  removeFileFn() {
+    return this.removeFile.bind(this);
+  },
+  removeFile(viewmodel) {
+    const { _id, url } = viewmodel.getData();
+    const fileUploader = this.fileUploader();
+
+    const isFileUploading = fileUploader.isFileUploading(_id);
+
+    let warningMsg = 'This file will be removed';
+    let buttonText = 'Remove';
+    if (isFileUploading) {
+      warningMsg = 'The upload process will be canceled';
+      buttonText = 'OK';
+    }
+
+    swal({
+      title: 'Are you sure?',
+      text: warningMsg,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonText: buttonText,
+      closeOnConfirm: true
+    }, () => {
+      if (isFileUploading) {
+        fileUploader.cancelUpload(_id);
+      }
+
+      const options = {
+        $pull: {
+          files: { _id }
+        }
+      };
+
+      this.parent().update({ options });
+    });
+  },
+  uploaderMetaContext: {}
+});
