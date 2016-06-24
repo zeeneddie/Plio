a71745807118354e4770b0338f9900ea416d76b8
Michael Rokosh <mike.rokosh@gmail.com>
2016-06-24 19:53:50 +0300

    Local sweetalert package

--- app/packages/meteor-bootstrap-sweetalert2/package.js
+++ app/packages/meteor-bootstrap-sweetalert2/package.js
@@ -1,20 +1,24 @@
 Package.describe({
-    name: 'codechimera:meteor-bootstrap-sweetalert',
-    version: '0.0.1',
+    name: 'plio:bootstrap-sweetalert',
+    version: '1.0.0',
+
     // Brief, one-line summary of the package.
     summary: 'A beautiful (bootstrap-styled) "replacement" for JavaScript\'s alert',
+
     // URL to the Git repository containing the source code for this package.
     git: 'https://github.com/codechimera/meteor-bootstrap-sweetalert.git',
+    
     // By default, Meteor will default to using README.md for documentation.
     // To avoid submitting documentation, set this field to null.
     documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.0.3.2');
+    api.versionsFrom('1.3.2.1');
+    api.use('ecmascript', 'client');
+
     api.addFiles([
-        'bootstrap-sweetalert/lib/sweet-alert.html',
-        'bootstrap-sweetalert/lib/sweet-alert.css',
-        'bootstrap-sweetalert/lib/sweet-alert.js'
+        'bootstrap-sweetalert/dist/sweetalert.css',
+        'bootstrap-sweetalert/dev/sweetalert.es6.js'
     ], ['client']);
 });
