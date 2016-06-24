608e9dae72b1e3cda2eb02ed2cc93807f6d81ffb
RevelsGit <revels0k@yandex.ru>
2016-06-24 13:04:04 +0300

    error handling:

--- app/imports/ui/components/includes/subcards/non-conformities/edit/non-conformities.js
+++ app/imports/ui/components/includes/subcards/non-conformities/edit/non-conformities.js
@@ -28,6 +28,10 @@ Template.Subcards_NonConformities_Edit.viewmodel({
     const organizationId = this.organizationId();
 
     const cb = (err, _id) => {
+      if (err) {
+        callback(err, _id);
+        return;
+      }
       const newNCSubcard = ViewModel.findOne('SubCardEdit', vm => vm._id && vm._id() === _id);
       newNCSubcard && newNCSubcard.toggleCollapse();
       callback(err, _id);
@@ -62,8 +66,14 @@ Template.Subcards_NonConformities_Edit.viewmodel({
           closeOnConfirm: false
         },
         () => {
-          const cb = () => {
+          const cb = (err) => {
+            if (err) {
+              swal.close();
+              return;
+            }
+
             viewmodel.destroy();
+
             swal('Removed!', `The non-conformity "${title}" was removed successfully.`, 'success');
           };
 
