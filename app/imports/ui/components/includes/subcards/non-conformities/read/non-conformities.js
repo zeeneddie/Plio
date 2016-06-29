fd73d745f16a898d177a33d5f413b5b7372b215f
RevelsGit <revels0k@yandex.ru>
2016-06-29 14:15:50 +0300

    fixed colors in ncs read subcard

--- app/imports/ui/components/includes/subcards/non-conformities/read/non-conformities.js
+++ app/imports/ui/components/includes/subcards/non-conformities/read/non-conformities.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 
 Template.Subcards_NonConformities_Read.viewmodel({
-  mixin: ['organization', 'nonconformity', 'NCStatus'],
+  mixin: ['organization', 'nonconformity', 'problemsStatus'],
   _query: {},
   NCs() {
     return this._getNCsByQuery({ ...this._query() });
