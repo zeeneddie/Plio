f3b93d6004b563cea90bcc0b5f70e7a25c2b28f8
RevelsGit <revels0k@yandex.ru>
2016-07-06 17:18:33 +0300

    fixed departments field

--- app/imports/ui/components/includes/fields/departments/edit/departments.js
+++ app/imports/ui/components/includes/fields/departments/edit/departments.js
@@ -1,69 +1,3 @@
-// import { Template } from 'meteor/templating';
-// import { Blaze } from 'meteor/blaze';
-//
-// import { Departments } from '/imports/api/departments/departments.js';
-//
-// Template.Departments_Edit.viewmodel({
-//   mixin: ['search', 'organization'],
-//   departmentsIds: [],
-//   departmentsIdsArray() {
-//     return Array.from(this.departmentsIds() || []);
-//   },
-//   departmentSearchText() {
-//     const child = this.child('SelectItem');
-//     return child && child.value();
-//   },
-//   departments() {
-//     const ids = this.departmentsIdsArray();
-//     const query = {
-//       ...this.searchObject('departmentSearchText', 'name'),
-//       organizationId: this.organizationId(),
-//       _id: { $nin: ids }
-//     };
-//
-//     return Departments.find(query).map(({ name, _id }) => ({ title: name, name, _id }));
-//   },
-//   currentDepartments() {
-//     const ids = this.departmentsIdsArray();
-//     const query = { _id: { $in: ids }, organizationId: this.organizationId() };
-//     return Departments.find(query);
-//   },
-//   update(departmentId, option, cb) {
-//     const options = {
-//       [`${option}`]: {
-//         departments: departmentId
-//       }
-//     };
-//
-//     this.parent().update({ options }, cb);
-//   },
-//   onSelectCb() {
-//     return this.onSelect.bind(this);
-//   },
-//   onSelect(viewmodel) {
-//     const { selected:departmentId } = viewmodel.getData();
-//
-//     if (this.departmentsIdsArray().find(id => id === departmentId)) return;
-//
-//     const cb = () => {
-//       viewmodel.value('');
-//       viewmodel.selected('');
-//     };
-//
-//     this.update(departmentId, '$addToSet', cb);
-//   },
-//   onRemoveCb() {
-//     return this.remove.bind(this);
-//   },
-//   remove(e) {
-//     const { _id } = Blaze.getData(e.target);
-//
-//     if (!this.departmentsIdsArray().find(id => id === _id)) return;
-//
-//     this.update(_id, '$pull');
-//   }
-// });
-
 import { Template } from 'meteor/templating';
 import { Blaze } from 'meteor/blaze';
 
@@ -74,23 +8,22 @@ Template.Departments_Edit.viewmodel({
   departmentsIds: [],
   selected() {
     const departmentsIds = Array.from(this.departmentsIds() || []);
-    const query = { organizationId: this.organizationId(), _id: { $in: this.departmentsIds } };
+    const query = { organizationId: this.organizationId(), _id: { $in: departmentsIds } };
     const options = { sort: { name: 1 } };
-    return Departments.find(query).map(({ name, _id }) => ({ title: name, name, _id }));
+    return Departments.find(query, options).map(({ name, ...args }) => ({ title: name, name, ...args }));
   },
   value() {
     const child = this.child('Select_Multi');
     return child && child.value();
   },
   departments() {
-    const ids = this.departmentsIds();
     const query = {
       ...this.searchObject('value', 'name'),
-      organizationId: this.organizationId(),
-      _id: { $nin: ids }
+      organizationId: this.organizationId()
     };
+    const options = { sort: { name: 1 } };
 
-    return Departments.find(query).map(({ name, _id }) => ({ title: name, name, _id }));
+    return Departments.find(query, options).map(({ name, ...args }) => ({ title: name, name, ...args }));
   },
   onUpdateCb() {
     return this.update.bind(this);
@@ -98,14 +31,7 @@ Template.Departments_Edit.viewmodel({
   update(viewmodel) {
     const { selected = [] } = viewmodel.getData();
 
-    // const options = {
-    //   [`${option}`]: {
-    //     departments: departmentId
-    //   }
-    // };
-    //
-    // this.parent().update({ options }, cb);
-    if (selected.length === this.selected().count() &&  selected.every(({ _id:itemId }) => this.selected().fetch().find(({ _id }) => _id === itemId))) return;
+    if (selected.length === this.selected().length > 0 &&  selected.every(({ _id:itemId }) => this.selected().find(({ _id }) => _id === itemId))) return;
 
     const departmentsIds = selected.map(({ _id }) => _id);
 
@@ -113,7 +39,7 @@ Template.Departments_Edit.viewmodel({
 
     if (!this._id) return;
 
-    this.parent().update({ departmentsIds })
+    this.parent().update({ departmentsIds });
   },
   onRemoveCb() {
     return this.remove.bind(this);
@@ -121,21 +47,6 @@ Template.Departments_Edit.viewmodel({
   remove(viewmodel) {
     this.update(viewmodel);
   },
-  // onSelectCb() {
-  //   return this.onSelect.bind(this);
-  // },
-  // onSelect(viewmodel) {
-  //   // const { selected:departmentId } = viewmodel.getData();
-  //   //
-  //   // if (this.departmentsIdsArray().find(id => id === departmentId)) return;
-  //   //
-  //   // const cb = () => {
-  //   //   viewmodel.value('');
-  //   //   viewmodel.selected('');
-  //   // };
-  //   //
-  //   this.update(viewmodel);
-  // },
   getData() {
     const { departmentsIds } = this.data();
     return { departmentsIds };
