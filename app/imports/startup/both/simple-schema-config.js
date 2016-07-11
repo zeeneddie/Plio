252633625bc3fcbc9d63bbf96494568b580ba4cf
CandyOgre <no.one.coin@gmail.com>
2016-07-11 12:42:22 +0300

    User-friendly validation error for phone number

--- app/imports/startup/both/simple-schema-config.js
+++ app/imports/startup/both/simple-schema-config.js
@@ -1,6 +1,9 @@
 SimpleSchema.messages({
-  regEx: [
+  "regEx": [
     {msg: "Failed regular expression validation"},
     {exp: SimpleSchema.RegEx.Email, msg: '"[value]" must be a valid e-mail address'},
-  ]
+  ],
+  "regEx number": [
+    {msg: '"[value]" is not a valid phone number'},
+  ],
 });
