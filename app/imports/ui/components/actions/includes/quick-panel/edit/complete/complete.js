2cf754a2415b90c4f2b9b893aa90f513fb9a33df
RevelsGit <revels0k@yandex.ru>
2016-07-15 13:57:45 +0300

    quick panel refactored

--- app/imports/ui/components/actions/includes/quick-panel/edit/complete/complete.js
+++ app/imports/ui/components/actions/includes/quick-panel/edit/complete/complete.js
@@ -4,8 +4,13 @@ import { complete } from '/imports/api/actions/methods.js';
 
 Template.Actions_QAPanel_Edit_Complete.viewmodel({
   comments: '',
+  isAction: false,
   update() {
-    this.parent().update(complete, { ...this.getData() });
+    if (this.isAction()) {
+      this.parent().update(complete, { ...this.getData() });
+    } else {
+      // TODO method for root cause analysis
+    }
   },
   getData() {
     const { comments:completionComments } = this.data();
--- app/imports/ui/components/actions/includes/quick-panel/read/qa-panel.js
+++ app/imports/ui/components/actions/includes/quick-panel/read/qa-panel.js
@@ -1,14 +1,25 @@
 import { Template } from 'meteor/templating';
 import { Meteor } from 'meteor/meteor';
 
+import { AnalysisStatuses } from '/imports/api/constants.js';
+import { ActionDocumentTypes } from '/imports/api/constants.js';
+
 Template.Actions_QAPanel_Read.viewmodel({
   mixin: ['action', 'user', 'date', 'utils', 'modal'],
   document: '',
-  showQAPanel({ isVerified, isCompleted, toBeCompletedBy, toBeVerifiedBy }) {
-    return !isVerified && this.chooseOne(isCompleted)(toBeVerifiedBy, toBeCompletedBy) === Meteor.userId();
+  _documentType: '',
+  isAction() {
+    return this._documentType() === ActionDocumentTypes.ACTION;
   },
-  getActionButtonText({ isCompleted }) {
-    return this.chooseOne(isCompleted)('Verify', 'Complete');
+  showQAPanel({ isVerified, isCompleted, toBeCompletedBy, toBeVerifiedBy, analysis: { executor, status } = {} } ) {
+    const aCondition = !isVerified && this.chooseOne(isCompleted)(toBeVerifiedBy, toBeCompletedBy) === Meteor.userId();
+    const pCondition = status === 0 && executor === Meteor.userId();
+    return (this.isAction() && aCondition) || pCondition;
+  },
+  getActionButtonText({ isCompleted, analysis: { status } = {} }) {
+    const aText = isCompleted ? 'Verify' : 'Complete';
+    const pText = status === 0 ? 'Complete' : '';
+    return this.isAction() ? aText : pText;
   },
   getActionDescription({ type, isCompleted, toBeCompletedBy, toBeVerifiedBy, completionTargetDate, verificationTargetDate }) {
     const chooseOne = this.chooseOne(isCompleted);
@@ -19,15 +30,15 @@ Template.Actions_QAPanel_Read.viewmodel({
       ${this._getNameByType(type)} to be ${completedOrVerified} by ${this.userFullNameOrEmail(assignee)} by ${this.renderDate(targetDate)}
     `;
   },
-  openQAModal({ _id, isCompleted }) {
-    const _title = this.getActionButtonText({ isCompleted });
+  openQAModal({ _id, isCompleted, analysis, ...args }) {
+    const _title = this.getActionButtonText({ isCompleted, analysis });
     const content = `${this.templateName().replace('Read', 'Edit')}_${_title}`; // Example: Actions_QAPanel_Edit_Verify
     this.modal().open({
-      _id,
       _title,
       content,
+      document: { _id, isCompleted, analysis, ...args },
+      isAction: this.isAction(),
       closeCaption: 'Cancel',
-      guideHtml: 'Enter any verification comments below, then click either "Verified as effective" or "Assessed as ineffective"',
       template: 'Actions_QAPanel_Edit'
     });
   }
