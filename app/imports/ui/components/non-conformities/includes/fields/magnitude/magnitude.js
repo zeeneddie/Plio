f8de236bdb5615982bf0a223c44a6d0435a747ca
RevelsGit <revels0k@yandex.ru>
2016-07-20 15:38:00 +0300

    fixed collapsing bug for magnitude field

--- app/imports/ui/components/non-conformities/includes/fields/magnitude/magnitude.js
+++ app/imports/ui/components/non-conformities/includes/fields/magnitude/magnitude.js
@@ -24,10 +24,12 @@ Template.Problems_Magnitude_Edit.viewmodel({
     const cb = () => {
       this.expandCollapsed(this._id(), () => {
         // hack to get around collapsing bug if item from section which has only this item was transfered to another section which has multiple items
-        const vm = ViewModel.findOne('ListItem', viewmodel => this.findRecursive(viewmodel, this._id()));
-        if (vm && vm.collapsed()) {
-          vm.toggleCollapse();
-        }
+        Meteor.defer(() => {
+          const vm = ViewModel.findOne('ListItem', viewmodel => this.findRecursive(viewmodel, this._id()));
+          if (vm && vm.collapsed()) {
+            vm.toggleCollapse();
+          }
+        });
       });
     };
 
