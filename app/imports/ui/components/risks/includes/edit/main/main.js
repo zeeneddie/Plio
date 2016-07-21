96ebeb2be6a8892b7bf59dc406291fa9b53f8ede
Michael Rokosh <mike.rokosh@gmail.com>
2016-07-21 17:47:12 +0300

    Added main risk template

--- app/imports/ui/components/risks/includes/edit/main/main.js
+++ app/imports/ui/components/risks/includes/edit/main/main.js
@@ -3,14 +3,13 @@ import { Template } from 'meteor/templating';
 import { updateViewedBy } from '/imports/api/non-conformities/methods.js';
 import { isViewed } from '/imports/api/checkers.js';
 
-Template.NC_Card_Edit_Main.viewmodel({
+Template.Risk_Card_Edit_Main.viewmodel({
   mixin: 'organization',
-  isStandardsEditable: true,
   onRendered(templateInstance) {
-    const doc = templateInstance.data.NC;
+    const doc = templateInstance.data.risk;
     const userId = Meteor.userId();
 
-    if(!isViewed(doc, userId)) {
+    if(doc && !isViewed(doc, userId)) {
       updateViewedBy.call({ _id: doc._id });
     }
   },
@@ -19,11 +18,5 @@ Template.NC_Card_Edit_Main.viewmodel({
   },
   update(...args) {
     this.parent().update(...args);
-  },
-  getData() {
-    return this.children(vm => vm.getData)
-                .reduce((prev, cur) => {
-                  return { ...prev, ...cur.getData() };
-                }, {});
   }
 });
