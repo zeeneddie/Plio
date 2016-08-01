590b06e90a89f73656d00e839669237cd2752fcd
RevelsGit <revels0k@yandex.ru>
2016-08-01 13:50:26 +0300

    fixed improvement plan for work inbox screen

--- app/imports/ui/pages/work-inbox-page.js
+++ app/imports/ui/pages/work-inbox-page.js
@@ -9,11 +9,14 @@ Template.WorkInbox_Page.viewmodel({
     const template = this.templateInstance;
     const _id = this.organizationId();
     const NCIds = this._getNCsByQuery({}).map(({ _id }) => _id);
+    const { linkedDoc: { _id:linkedDocId } = {} } = this._getWorkItemByQuery({ _id: this.workItemId() }) || {};
 
     template.subscribe('lessons', _id);
     template.subscribe('departments', _id);
     template.subscribe('riskTypes', _id);
     template.subscribe('standards', _id);
     template.subscribe('occurrencesByNCIds', NCIds);
+    template.subscribe('NCImprovementPlan', linkedDocId);
+    template.subscribe('riskImprovementPlan', linkedDocId);
   }
 });
