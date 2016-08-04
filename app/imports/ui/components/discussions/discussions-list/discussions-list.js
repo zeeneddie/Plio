8ebbc45e9716afb2a3bbc3e748b97a0ccebabd39
Vasyl Samusiev <vasyl.samusiev@cultofcoders.com>
2016-08-04 10:45:10 +0300

    Message list grouping by date

--- app/imports/ui/components/discussions/discussions-list/discussions-list.js
+++ app/imports/ui/components/discussions/discussions-list/discussions-list.js
@@ -1,14 +1,32 @@
-import {moment} from 'meteor/momentjs:moment';
 import {Template} from 'meteor/templating';
 
 import {Discussions} from '/imports/api/discussions/discussion.js';
+import {getFormattedDate} from '/imports/api/helpers.js';
 
 
 Template.DiscussionsList.viewmodel({
 	mixin: ['standard'],
 
+	messageFirst(){
+		const self = this;
+		const m = Discussions.findOne({
+			standardId: self.standardId()
+		}, {
+			fields: {createdAt: 1, userId: 1},
+			limit: 1,
+			sort: {createdAt: 1}
+		});
+
+		return {
+			date: getFormattedDate(m.createdAt, 'MMMM Do, YYYY'),
+			name: Meteor.users.findOne({_id: m.userId}).fullName()
+		};
+	},
+
 	messages(){
 		const self = this;
+		let dateStorage;
+
 		return Discussions.find({
 			standardId: self.standardId()
 		}, {
@@ -16,11 +34,16 @@ Template.DiscussionsList.viewmodel({
 			sort: {createdAt: 1}
 		}).map((c, i, cr) => {
 			const user = Meteor.users.findOne({_id: c.userId}, {fields: {profile:1}});
-			console.dir(user);
+
 			c.avatar = user.avatar();
-			c.date = moment(c.createdAt).format('YYYY MMMM Do');
-			c.time = moment(c.createdAt).format('HH:mm');
+			c.date = getFormattedDate(c.createdAt, 'MMMM Do, YYYY');
+			c.dateToShow = dateStorage !== c.date;
+			c.time = getFormattedDate(c.createdAt, 'HH:mm');
 			c.username = user.firstName();
+
+			if(dateStorage !== c.date){
+				dateStorage = c.date;
+			}
 			console.dir(c);
 			return c;
 		});
