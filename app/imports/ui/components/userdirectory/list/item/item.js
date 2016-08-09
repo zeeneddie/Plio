a8c6b78799de3cfa0abd0dcf83a8cf7473941127
Michael Rokosh <mike@Michaels-Mac-mini.local>
2016-08-09 15:18:01 +0300

    Added hrefs to userdirectory items

--- app/imports/ui/components/userdirectory/list/item/item.js
+++ app/imports/ui/components/userdirectory/list/item/item.js
@@ -3,9 +3,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 
 Template.UserDirectory_List_Item.viewmodel({
   share: 'window',
-  mixin: 'user',
+  mixin: ['user', 'organization'],
   isActiveUser() {
     return FlowRouter.getParam('userId') === this._id();
+  },
+  getHref() {
+    const params = { orgSerialNumber: this.organizationSerialNumber(), userId: this._id() };
+    return FlowRouter.path('userDirectoryUserPage', params);
   },
   goToUser(e) {
     e.preventDefault();
