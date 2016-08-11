0c61a01c09f6041d47b41e8e422556c0830c048b
Catvas <catvaas@googlemail.com>
2016-08-11 18:30:37 +0300

    Checking work after merging with origin/devel. Solving bugs

--- app/imports/ui/layouts/standard-discussion-layout.js
+++ app/imports/ui/layouts/standard-discussion-layout.js
@@ -6,7 +6,8 @@ import { OrgSubs, UserSubs } from '/imports/startup/client/subsmanagers.js';
 
 Template.StandardDiscussionLayout.viewmodel({
 	mixin: ['organization', 'standard'],
-	_subHandlers: [],
+	//_subHandlers: [],
+	_subHandlers: {},
 	isOrganisation: true, // [ToDo] Check if the organisation exists?
 	isReady: false,
 	isStandart: true, // [ToDo] Check if the standart exists?
@@ -32,7 +33,7 @@ Template.StandardDiscussionLayout.viewmodel({
 			const { _id, users } = !!org && org;
       const userIds = _.pluck(users, 'userId');
 
-			const _subHandlers = [
+			/*const _subHandlers = [
 				OrgSubs.subscribe('currentUserOrganizationBySerialNumber', orgSerialNumber),
         UserSubs.subscribe('organizationUsers', userIds),
 				this.templateInstance.subscribe('discussionsByStandardId', standardId),
@@ -43,18 +44,53 @@ Template.StandardDiscussionLayout.viewmodel({
         this.templateInstance.subscribe('actions', _id),
         this.templateInstance.subscribe('nonConformities', _id),
         this.templateInstance.subscribe('risks', _id)
-			];
+			];*/
+
+			this._subHandlers.OrgSubs = OrgSubs.subscribe(
+				'currentUserOrganizationBySerialNumber', orgSerialNumber
+			);
+			this._subHandlers.UserSubs = UserSubs.subscribe(
+				'organizationUsers', userIds
+			);
+			this._subHandlers.discussionsByStandardId = this.templateInstance.subscribe(
+				'discussionsByStandardId', standardId
+			);
+			this._subHandlers.standardsBookSections = this.templateInstance.subscribe(
+				'standards-book-sections', _id
+			);
+			this._subHandlers.standardsTypes = this.templateInstance.subscribe(
+				'standards-types', _id
+			);
+			this._subHandlers.lessons = this.templateInstance.subscribe(
+				'lessons', _id
+			);
+			this._subHandlers.actions = this.templateInstance.subscribe(
+				'actions', _id
+			);
+			this._subHandlers.nonConformities = this.templateInstance.subscribe(
+				'nonConformities', _id
+			);
+			this._subHandlers.risks = this.templateInstance.subscribe('risks', _id);
 
 			if (this.isActiveStandardFilter('deleted')) {
-        _subHandlers.push(this.templateInstance.subscribe('standardsDeleted', _id));
+        //_subHandlers.push(this.templateInstance.subscribe('standardsDeleted', _id));
+				this._subHandlers.standardsDeleted = this.templateInstance.subscribe(
+					'standardsDeleted', _id
+				);
       } else {
-        _subHandlers.push(this.templateInstance.subscribe('standards', _id));
+        //_subHandlers.push(this.templateInstance.subscribe('standards', _id));
+				this._subHandlers.standards = this.templateInstance.subscribe(
+					'standards', _id
+				);
       }
 
-			this._subHandlers(_subHandlers);
+			//this._subHandlers(_subHandlers);
 		},
 		function(){
-			if( !this._subsHandlers[2].ready() ){
+			/*
+			let _subHandlers = this._subHandlers.value;
+
+			if( !_subHandlers[2] || !_subHandlers[2].ready() ){
 				return;
 			}
 
@@ -66,10 +102,30 @@ Template.StandardDiscussionLayout.viewmodel({
 				'messagesByDiscussionIds', discussionIds
 			)
 
-			this._subsHandlers.push(messagesHandler);
+			_subHandlers.push(messagesHandler);
+			this._subHandlers(_subHandlers);
+			*/
+
+			if( !this._subHandlers.discussionsByStandardId.ready() ){
+				return;
+			}
+
+			const discussionIds = Discussions.find({}, { fields: {_id: 1} })
+				.map((c) => {
+					return c._id;
+				});
+
+			this._subHandlers.messagesHandler = this.templateInstance.subscribe(
+				'messagesByDiscussionIds', discussionIds
+			);
 		},
 		function(){
-			this.isReady( this._subHandlers().every( handle => handle.ready() ) );
+			//this.isReady( this._subHandlers().every( handle => handle.ready() ) );
+			const s = this._subHandlers();
+			this.isReady(
+				_.values( s ).every( handle => handle.ready() )
+			);
+			console.dir(s);
 //>>>>>>> origin/devel
 		}
 	],
