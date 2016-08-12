4509f26876c3e402e348dcb63ced2d238adfc767
Michael Rokosh <mike@Michaels-Mac-mini.local>
2016-08-12 16:44:43 +0300

    Fixed few ui things

--- app/imports/startup/client/subsmanagers.js
+++ app/imports/startup/client/subsmanagers.js
@@ -2,10 +2,13 @@ const OrgSubs = new SubsManager();
 const UserSubs = new SubsManager({
 
   // subscriptions will expire after 20 minutes, if they won't be subscribed again
-  expireIn: 10
+  expireIn: 20
 });
 const CountSubs = new SubsManager({
-  expireIn: 5
+  expireIn: 10
+});
+const WorkItemSubs = new SubsManager({
+  expireIn: 10
 });
 
-export { OrgSubs, UserSubs, CountSubs };
+export { OrgSubs, UserSubs, CountSubs, WorkItemSubs };
