04d414560467cfea08d68139932317a56e1fb0f5
Michael Rokosh <mike@Michaels-Mac-mini.local>
2016-08-19 19:17:25 +0300

    File uploader redesign & fix

--- app/imports/ui/components/includes/file-item/file-item-read/file-item-read.js
+++ app/imports/ui/components/includes/file-item/file-item-read/file-item-read.js
@@ -1,7 +1,7 @@
 Template.FileItem_Read.viewmodel({
   autorun() {
     if (!this.isUploaded()) {
-      const progressWidth = 100 - this.progress();
+      const progressWidth = this.progress();
 
       this.templateInstance.$('.uploading-file').css({ 'width':  progressWidth + '%' });
     }
