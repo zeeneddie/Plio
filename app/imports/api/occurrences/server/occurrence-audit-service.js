89b4d8d6e56dc18e4cfe55d4952f0016467f9ca8
meyginix <meyginix@gmail.com>
2016-08-22 17:39:11 +0300

    occurrence audit

--- app/imports/api/occurrences/server/occurrence-audit-service.js
+++ app/imports/api/occurrences/server/occurrence-audit-service.js
@@ -6,5 +6,29 @@ import OccurrenceUpdateAudit from '/imports/core/audit/server/OccurrenceUpdateAu
 export default _.extend({}, AuditService, {
   _collection: CollectionNames.OCCURRENCES,
 
-  _updateAuditConstructor: OccurrenceUpdateAudit
+  _updateAuditConstructor: OccurrenceUpdateAudit,
+
+  _onDocumentCreated(newDocument, userId) {
+    const { createdAt, createdBy, nonConformityId } = newDocument;
+
+    this._saveLogs([{
+      documentId: nonConformityId,
+      collection: CollectionNames.NCS,
+      message: 'Occurrence added',
+      date: createdAt,
+      executor: createdBy
+    }]);
+  },
+
+  _onDocumentRemoved(oldDocument, userId) {
+    const { nonConformityId } = oldDocument;
+
+    this._saveLogs([{
+      documentId: nonConformityId,
+      collection: CollectionNames.NCS,
+      message: 'Occurrence removed',
+      date: new Date(),
+      executor: userId
+    }]);
+  }
 });
--- app/imports/core/audit/server/OccurrenceUpdateAudit.js
+++ app/imports/core/audit/server/OccurrenceUpdateAudit.js
@@ -9,6 +9,20 @@ const base = Utils.inherit(DocumentUpdateAudit, [DescriptionUpdateAudit]);
 
 export default class OccurrenceUpdateAudit extends base {
 
+  _createLog({ message, ...rest }) {
+    super._createLog({ message, ...rest });
+
+    const lowerCaseMessage = message[0].toLowerCase() + message.slice(1);
+
+    this._logs.push({
+      documentId: this._newDoc.nonConformityId,
+      collection: CollectionNames.NCS,
+      message: `Occurrence ${lowerCaseMessage}`,
+      date: this._updatedAt,
+      executor: this._updatedBy
+    });
+  }
+
   static get _fieldLabels() {
     const fieldLabels = {
       date: 'Date',
