e71cf48c3143370e43e9d95fa9785085eb09cc9c
Michael Rokosh <mike@rokosh.local>
2016-09-15 12:13:22 +0300

    Fixed recap table width

--- app/imports/startup/server/mail.js
+++ app/imports/startup/server/mail.js
@@ -2,6 +2,6 @@ import Utils from '/imports/core/utils';
 
 const {login, password, hostname, port} = Meteor.settings.mail;
 
-if (Utils.isProduction()) {
+// if (Utils.isProduction()) {
   process.env.MAIL_URL = `smtp://${login}:${password}@${hostname}:${port}`;
-}
+// }
