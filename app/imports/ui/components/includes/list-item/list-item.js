533edc20270f85455aa25ff2c1a20e062c020898
Michael Rokosh <mike@MacBook.local>
2016-09-23 20:36:49 +0300

    Fixed collapsible arrow bug

--- app/imports/ui/components/includes/list-item/list-item.js
+++ app/imports/ui/components/includes/list-item/list-item.js
@@ -6,6 +6,7 @@ Template.ListItem.viewmodel({
   share: ['search'],
   mixin: ['collapse', 'search', 'standard'],
   rText: '',
+  collapsed: true,
   hideRTextOnExpand: false,
   isRTextDisplayed() {
     return this.rText() && (!this.hideRTextOnExpand() || (this.collapsed() && this.hideRTextOnExpand()));
