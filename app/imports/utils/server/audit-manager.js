69cf355ead2210d2b09781d30b8558d203892c17
meyginix <meyginix@gmail.com>
2016-09-29 18:59:26 +0300

    audit in background app

--- app/imports/utils/server/audit-manager.js
+++ app/imports/utils/server/audit-manager.js
@@ -1,23 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
+import BaseAuditManager from '/imports/share/utils/base-audit-manager.js';
 import BackgroundApp from './background-app.js';
 
 
-export default AuditManager = {
-
-  _isAuditStarted: false,
-
-  startAudit() {
-    this._isAuditStarted = true;
-  },
-
-  stopAudit() {
-    this._isAuditStarted = false;
-  },
-
-  isAuditStarted() {
-    return this._isAuditStarted === true;
-  },
+export default AuditManager = _.extend({}, BaseAuditManager, {
 
   documentCreated(newDocument, userId, collectionName) {
     BackgroundApp.call('documentCreated', {
@@ -35,34 +22,6 @@ export default AuditManager = {
     BackgroundApp.call('documentRemoved', {
       oldDocument, userId, collectionName
     });
-  },
-
-  registerCollection(collection, collectionName) {
-    const auditManager = this;
-
-    collection.after.insert(function(userId, doc) {
-      Meteor.defer(() => {
-        if (auditManager.isAuditStarted()) {
-          auditManager.documentCreated(doc, userId, collectionName);
-        }
-      });
-    });
-
-    collection.after.update(function(userId, doc, fieldNames, modifier, options) {
-      Meteor.defer(() => {
-        if (auditManager.isAuditStarted()) {
-          auditManager.documentUpdated(doc, this.previous, userId, collectionName);
-        }
-      });
-    });
-
-    collection.after.remove(function(userId, doc) {
-      Meteor.defer(() => {
-        if (auditManager.isAuditStarted()) {
-          auditManager.documentRemoved(doc, userId, collectionName);
-        }
-      });
-    });
   }
 
-};
+});
--- background-app/imports/startup/index.js
+++ background-app/imports/startup/index.js
@@ -1,6 +1,7 @@
 import './handlebars-cache.js';
 import './mail.js';
 import './users.js';
-import './audit.js';
+import './audit-configs.js';
+import './audit-collections.js';
 import './cron';
 import '../api/audit-methods.js';
