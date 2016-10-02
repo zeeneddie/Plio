29d1cbe1ed2a0b28760efb4720efea3352e9c808
meyginix <meyginix@gmail.com>
2016-10-02 20:17:30 +0300

    background job for workflow updates

--- background-app/imports/startup/cron/workflow.js
+++ background-app/imports/startup/cron/workflow.js
@@ -1,11 +1,6 @@
-import { Actions } from '/imports/share/collections/actions.js';
-import { NonConformities } from '/imports/share/collections/non-conformities.js';
-import { Risks } from '/imports/share/collections/risks.js';
-import { WorkflowTypes } from '/imports/share/constants.js';
+import moment from 'moment-timezone';
 
-//import ActionWorkflow from '/imports/core/workflow/server/ActionWorkflow.js';
-//import NCWorkflow from '/imports/core/workflow/server/NCWorkflow.js';
-//import RiskWorkflow from '/imports/core/workflow/server/RiskWorkflow.js';
+import WorkflowUpdater from '/imports/workflow/WorkflowUpdater.js';
 
 
 SyncedCron.add({
@@ -16,41 +11,12 @@ SyncedCron.add({
   },
 
   job() {
-    /*const actions = Actions.find({
-      $or: [{
-        isCompleted: false,
-        completedAt: { $exists: false },
-        completedBy: { $exists: false }
-      }, {
-        isCompleted: true,
-        completedAt: { $exists: true },
-        completedBy: { $exists: true },
-        isVerified: false,
-        verifiedAt: { $exists: false },
-        verifiedBy: { $exists: false },
-        status: { $ne: 9 } // Completed
-      }]
+    const timezones = _(moment.tz.names()).filter((name) => {
+      return moment().tz(name).hours() === 0;
     });
 
-    actions.forEach(doc => new ActionWorkflow(doc).refreshStatus());
-
-    const problemDocQuery = {
-      workflowType: WorkflowTypes.SIX_STEP,
-      $or: [{
-        'analysis.status': 0, // Not completed
-        'analysis.completedAt': { $exists: false },
-        'analysis.completedBy': { $exists: false }
-      }, {
-        'updateOfStandards.status': 0, // Not completed
-        'updateOfStandards.completedAt': { $exists: false },
-        'updateOfStandards.completedBy': { $exists: false }
-      }]
-    };
-
-    const NCs = NonConformities.find(problemDocQuery);
-    NCs.forEach(doc => new NCWorkflow(doc).refreshStatus());
-
-    const risks = Risks.find(problemDocQuery);
-    risks.forEach(doc => new RiskWorkflow(doc).refreshStatus());*/
+    Organizations.find({ timezone: { $in: timezones } }).forEach((org) => {
+      new WorkflowUpdater(org._id).update();
+    });
   }
 });
