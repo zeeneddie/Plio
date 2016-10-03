2aee43219ac139d3c120bbecf3c25500d5760d9b
Michael Rokosh <mike@rokosh.local>
2016-10-03 18:48:39 +0300

    I121, I130, I118, I116, I112

--- app/imports/api/constants.js
+++ app/imports/api/constants.js
@@ -259,6 +259,10 @@ const OrganizationDefaults = {
 
 const DefaultStandardTypes = [
   {
+    title: 'Process',
+    abbreviation: 'PRO'
+  },
+  {
     title: 'Policy',
     abbreviation: 'POL'
   },
@@ -267,28 +271,31 @@ const DefaultStandardTypes = [
     abbreviation: 'CHK'
   },
   {
-    title: 'Standard Operating Procedure',
+    title: 'Compliance management objective',
+    abbreviation: 'CMO'
+  },
+  {
+    title: 'Compliance obligation',
+    abbreviation: 'COB'
+  },
+  {
+    title: 'Standard operating procedure',
     abbreviation: 'SOP'
   },
   {
     title: 'Work instruction',
-    abbreviation: 'WRK'
+    abbreviation: 'WORK'
   },
   {
     title: 'Product specification',
-    abbreviation: 'SPC'
+    abbreviation: 'SPEC'
   },
   {
-    title: 'Test method',
-    abbreviation: 'TST'
+    title: 'Risk control',
+    abbreviation: 'RSC'
   },
   {
-    title: 'Regulation',
-    abbreviation: 'REG'
-  },
-  {
-    title: 'Other',
-    abbreviation: 'DOC'
+    title: 'Section header'
   }
 ];
 
--- app/imports/ui/components/userdirectory/includes/preferences/preferences.js
+++ app/imports/ui/components/userdirectory/includes/preferences/preferences.js
@@ -63,33 +63,9 @@ Template.UserPreferences.viewmodel({
     return Sounds;
   },
   sendTestNotification() {
-    const quotes = [
-      `It is certain`,
-      `It is decidedly so`,
-      `Without a doubt`,
-      `Yes — definitely`,
-      `You may rely on it`,
-      `As I see it, yes`,
-      `Most likely`,
-      `Outlook good`,
-      `Signs point to yes`,
-      `Yes`,
-      `Reply hazy, try again`,
-      `Ask again later`,
-      `Better not tell you now`,
-      `Cannot predict now`,
-      `Concentrate and ask again`,
-      `Don’t count on it`,
-      `My reply is no`,
-      `My sources say no`,
-      `Outlook not so good`,
-      `Very doubtful`
-    ];
-
     this.sendNotification({
       title: 'Test notification',
-      body: Random.choice(quotes),
-      icon: '/p-logo-magic-8.png'
+      icon: '/p-logo-square.png'
     });
   },
   events: {
