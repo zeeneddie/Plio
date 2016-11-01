0ac5be1e85108f1ceacd5498325b07754ecd0d5d
Michael Rokosh <mike@rokosh.local>
2016-11-01 13:38:48 +0200

    Refactored

--- app/imports/ui/components/userdirectory/card/inner/inner.js
+++ app/imports/ui/components/userdirectory/card/inner/inner.js
@@ -25,12 +25,14 @@ Template.UserDirectory_Card_Read_Inner.viewmodel({
   },
   orgOwnerLabel() {
     const userId = this.user()._id;
+    const user = this.user();
+    const firstNameOrEmail = user && user.firstName() || 'This user';
     const organization = this.organization();
 
     if (userId && organization) {
       const orgName = organization.name;
       if (userId === organization.ownerId()) {
-        return `Organization owner for organization "${orgName}"`;
+        return `${firstNameOrEmail} is the organization owner for organization ${orgName} and has the full set of superpowers`;;
       }
     }
   },
