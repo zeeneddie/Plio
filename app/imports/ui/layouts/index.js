44a065606d11a1d48745eedf84cfead4cce3befc
RevelsGit <revels0k@yandex.ru>
2016-12-01 16:10:23 +0200

    removed unnecessary files

--- app/imports/ui/layouts/index.js
+++ app/imports/ui/layouts/index.js
@@ -1,8 +1,6 @@
 import './dashboard-layout.html';
 import './dashboard-layout.js';
 import './login-layout.html';
-import './standards-layout.html';
-import './standards-layout.js';
 import './user-directory-layout.html';
 import './user-directory-layout.js';
 import './transitional-layout.html';
