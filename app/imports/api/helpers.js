279af1176cdda3c23d18a531d29fd1d0cea06784
meyginix <meyginix@gmail.com>
2016-12-26 14:02:33 +0200

    added new workflow status

--- app/imports/api/helpers.js
+++ app/imports/api/helpers.js
@@ -398,20 +398,21 @@ export const getProblemStatusColor = (status) => {
     case 6:
     case 7:
     case 8:
-    case 10:
+    case 9:
     case 11:
     case 12:
-    case 14:
+    case 13:
     case 15:
+    case 16:
       return 'amber';
     case 5:
-    case 9:
-    case 13:
-    case 16:
+    case 10:
+    case 14:
     case 17:
-      return 'red';
     case 18:
+      return 'red';
     case 19:
+    case 20:
       return 'green';
     default:
       return '';
