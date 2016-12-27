98272b2de9481eb3a6335d442bae748c07cab443
RevelsGit <revels0k@yandex.ru>
2016-12-27 12:19:55 +0200

    functionality to unsubscribe from document's notify list

---
+++ app/imports/api/notifications/errors.js
@@ -0,0 +1,1 @@
+export const DOC_NOT_FOUND_OR_ALREADY_UNSUBSCRIBED = 'The document you are looking for is not found or you\'ve been already unsubscribed from notifications of this document';
--- app/imports/ui/react/pages/NotificationsUnsubscribePage/index.js
+++ app/imports/ui/react/pages/NotificationsUnsubscribePage/index.js
@@ -1,22 +1,41 @@
-import { compose, lifecycle, renderNothing, setPropTypes } from 'recompose';
-import { toastr } from 'meteor/chrismbeckett:toastr';
+import { compose, lifecycle, withState, flattenProp } from 'recompose';
 import React, { PropTypes } from 'react';
 
-const unsubscribe = ({
-  call: (_, cb) => setTimeout(() => cb(null), 1500),
-});
+import { handleMethodResult } from '/imports/api/helpers';
+import { getPath } from '/imports/ui/utils/router/paths';
+import { unsubscribe } from '/imports/api/notifications/methods';
 
-export default compose(
-  setPropTypes({
-    documentId: PropTypes.string.isRequired,
-    documentType: PropTypes.string.isRequired,
-  }),
+const enhance = compose(
+  withState('state', 'setState', { loading: true, error: null }),
   lifecycle({
     componentDidMount() {
       const { documentId, documentType } = this.props;
-      console.log(documentId, documentType)
-      unsubscribe.call({ documentId, documentType }, (err) =>
-        !err && toastr.info("You've been unsubscribed from notifications of this document"));
+      unsubscribe.call({ documentId, documentType }, handleMethodResult((error) =>
+        this.props.setState({ error, loading: false })
+      ));
     },
   }),
-)(() => <div>Hello World</div>);
+  flattenProp('state'),
+);
+
+const NotificationsUnsubscribePage = enhance(({ loading, error }) => {
+  if (loading) return (<span>Unsubscribing...</span>);
+
+  let text = "You've been successfully unsubscribed from notifications of this document!";
+
+  if (error) text = 'Ooops... something went wrong!';
+
+  return (
+    <div>
+      <h3>{text}</h3>
+      <a href={getPath('dashboardPage')()}>Open the dashboard</a>
+    </div>
+  );
+});
+
+NotificationsUnsubscribePage.propTypes = {
+  documentId: PropTypes.string.isRequired,
+  documentType: PropTypes.string.isRequired,
+};
+
+export default NotificationsUnsubscribePage;
