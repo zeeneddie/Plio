26661dbd849eabf4ee197f5293877e3f2bf95a30
Michael Rokosh <mike@rokosh.local>
2017-01-10 12:27:37 +0200

    Changed data export titles

--- app/imports/ui/react/non-comformities/components/HeaderMenu/HeaderMenu.js
+++ app/imports/ui/react/non-comformities/components/HeaderMenu/HeaderMenu.js
@@ -21,7 +21,7 @@ const dataExportProps = {
 const enhance = withState('isOpen', 'setIsOpen', false);
 const HeaderMenu = (props) => (
   <HeaderOptionsMenu {...props}>
-    <DataExportModal title="Non-conformities export" {...dataExportProps}>
+    <DataExportModal title="Data export" {...dataExportProps}>
       <DropdownItem>Export Data</DropdownItem>
     </DataExportModal>
   </HeaderOptionsMenu>
