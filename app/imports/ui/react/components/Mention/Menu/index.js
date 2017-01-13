8484dabc3a0bba949c7c25f49025e9a983a0e450
RevelsGit <revels0k@yandex.ru>
2017-01-13 13:50:08 +0200

    keyboard navigation support for mentions

--- app/imports/ui/react/components/Mention/Menu/index.js
+++ app/imports/ui/react/components/Mention/Menu/index.js
@@ -1,33 +1,53 @@
 import React, { PropTypes } from 'react';
-import { DropdownMenu, DropdownItem } from 'reactstrap';
+import { DropdownMenu } from 'reactstrap';
+import { compose, withState, withHandlers, defaultProps, lifecycle } from 'recompose';
+import property from 'lodash.property';
+import cx from 'classnames';
 
-import MessageAvatar from '../../../discussion/components/MessageAvatar';
+import MenuItem from '../MenuItem';
+import { handleKeyDown } from './handlers';
+import { omitProps } from '/imports/api/helpers';
 
-const MentionMenu = ({ users, onUserClick }) => (
-  <DropdownMenu className="dropdown-menu-full" role="menu">
-    {users.map((user) => (
-      <DropdownItem
-        href="#"
-        key={user.value}
-        tag="a"
-        onClick={e => onUserClick(e, user)}
-        onKeyPress={e => e.key === 'Enter' && onUserClick(e, user)}
-      >
-        <MessageAvatar tag="div" avatar={user.avatar} />
-        <span>{user.text} </span>
-        <span className="text-muted">{user.email}</span>
-      </DropdownItem>
-    ))}
-  </DropdownMenu>
+// BUG: when menu is opened and user manually focuses
+// input and types something the input loses focus
+
+const enhance = compose(
+  defaultProps({ focused: null }),
+  withState('focused', 'setFocus', property('focused')),
+  lifecycle({
+    componentWillReceiveProps({ users, setFocus }) {
+      if (this.props.users.length !== 0 && users.length === 0) setFocus(null);
+    },
+  }),
+  withHandlers({ onKeyDown: handleKeyDown }),
+  omitProps(['setFocus']),
 );
 
+const MentionMenu = enhance(({
+  users,
+  onUserSelect,
+  onKeyDown,
+  focused,
+  className,
+  Child = MenuItem,
+  ...other
+}) => (
+  <DropdownMenu className={cx(className, 'dropdown-menu-full')} {...{ onKeyDown, ...other }}>
+    {users.map((user, index) => (
+      <Child key={user.value} focused={focused === index} {...{ user, onUserSelect }} />
+    ))}
+  </DropdownMenu>
+));
+
 MentionMenu.propTypes = {
-  onUserClick: PropTypes.func,
+  focused: PropTypes.number,
+  onUserSelect: PropTypes.func,
   users: PropTypes.arrayOf(PropTypes.shape({
     text: PropTypes.string.isRequired,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     email: PropTypes.string.isRequired,
   })),
+  Child: PropTypes.func,
 };
 
 export default MentionMenu;
--- app/imports/ui/react/discussion/components/MessageAvatar/index.js
+++ app/imports/ui/react/discussion/components/MessageAvatar/index.js
@@ -1,20 +1,18 @@
 import React, { PropTypes } from 'react';
 
-const MessageAvatar = ({ avatar, alt, onClick, tag = 'a', ...other }) => {
+const MessageAvatar = ({ tag = 'a', children, ...other }) => {
   const Tag = tag;
 
   return (
-    <Tag {...{ ...other, onClick }} className="chat-item-avatar img-wrapper sm">
-      <img tabIndex="0" src={avatar} alt={alt} />
+    <Tag {...{ ...other }} className="chat-item-avatar img-wrapper sm">
+      {children}
     </Tag>
   );
 };
 
 MessageAvatar.propTypes = {
-  avatar: PropTypes.string.isRequired,
-  alt: PropTypes.string,
   tag: PropTypes.string,
-  onClick: PropTypes.func,
+  children: PropTypes.node,
 };
 
 export default MessageAvatar;
