544fba3d98d312e6b72875e9af0e933fceb74488
RevelsGit <revels0k@yandex.ru>
2017-01-16 15:45:50 +0200

    ...

--- app/imports/ui/components/notifications/notifications.js
+++ app/imports/ui/components/notifications/notifications.js
@@ -19,7 +19,6 @@ Template.Notifications.viewmodel({
     Notifications.find().observe({
       added: (doc) => {
         this.sendNotification(doc);
-        console.log(doc);
         Meteor.defer(() => updateViewedBy.call({ _id: doc._id }));
       }
     });
