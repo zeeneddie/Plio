4802381198df1d91d98d3e830e91a86195d66e75
RevelsGit <revels0k@yandex.ru>
2017-01-25 17:44:59 +0200

    discussion options

--- app/imports/ui/react/components/Icons/index.js
+++ app/imports/ui/react/components/Icons/index.js
@@ -1,4 +1,3 @@
 export { default as Icon } from './Icon';
 export { default as IconLoading } from './IconLoading';
 export { default as IconStack } from './IconStack';
-export { default as VolumeMute } from './VolumeMute';
