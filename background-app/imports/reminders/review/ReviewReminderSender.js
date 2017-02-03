481fed0db24b3cbf0d7e941aed2ab2f9f2c36d69
RevelsGit <revels0k@yandex.ru>
2017-02-03 17:25:14 +0200

    added untracked files

---
+++ background-app/imports/reminders/review/ReviewReminderSender.js
@@ -0,0 +1,159 @@
+/* eslint-disable new-cap */
+
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import moment from 'moment-timezone';
+
+import { Organizations } from '/imports/share/collections/organizations';
+import { Risks } from '/imports/share/collections/risks';
+import { Standards } from '/imports/share/collections/standards';
+import ReviewWorkflow from '/imports/share/utils/ReviewWorkflow';
+import { reviewConfigSchema } from '/imports/share/schemas/organization-schema';
+import { DocumentTypes } from '/imports/share/constants';
+import { capitalize, getDocTypePlural } from '/imports/share/helpers';
+import { getCollectionUrlByDocType } from '../../helpers/url';
+import { isDateScheduled, getPrettyTzDate } from '../../helpers/date';
+import NotificationSender from '/imports/share/utils/NotificationSender';
+
+const REMINDER_EMAIL_TEMPLATE = 'defaultEmail';
+
+export default class ReviewReminderSender {
+  constructor(organizationId) {
+    this._organizationId = organizationId;
+  }
+
+  _prepare() {
+    const query = { _id: this._organizationId };
+    const organization = Organizations.findOne(query);
+    if (!organization) {
+      throw new Error('Organization does not exist');
+    }
+
+    this._organization = organization;
+    this._timezone = organization.timezone || 'UTC';
+
+    this._date = moment()
+      .tz(this._timezone)
+      .startOf('day')
+      .toDate();
+
+    this._reminders = [];
+  }
+
+  send() {
+    this._prepare();
+
+    const { review } = this._organization;
+
+    this._checkReviewConfiguration(review);
+
+    this._createReviewReminder(Risks, review.risks, DocumentTypes.RISK);
+    this._createReviewReminder(Standards, review.standards, DocumentTypes.STANDARD);
+
+    this._sendReminders();
+  }
+
+  _checkReviewConfiguration(reviewConfig) {
+    const schema = new SimpleSchema({
+      standards: { type: reviewConfigSchema },
+      risks: { type: reviewConfigSchema },
+    });
+
+    try {
+      schema.validate(reviewConfig);
+    } catch (err) {
+      throw new Error(`Review config is not valid: ${err}`);
+    }
+  }
+
+  _createReviewReminder(collection, reviewConfig, docType) {
+    if (this._shouldSendReminder(collection, reviewConfig)) {
+      this._reminders.push({ collection, reviewConfig, docType });
+    }
+  }
+
+  _shouldSendReminder(collection, reviewConfig) {
+    const isScheduled = isDateScheduled(
+      reviewConfig.reminders,
+      reviewConfig.annualDate,
+      this._timezone,
+      this._date,
+    );
+
+    if (!isScheduled) return false;
+
+    const isStatusAwaitingReview = (doc) => {
+      const workflow = new ReviewWorkflow(doc, reviewConfig, this._timezone);
+
+      return workflow && workflow.getStatus && workflow.getStatus() === 1;
+    };
+
+    const query = {
+      organizationId: this._organizationId,
+      isDeleted: false,
+      deletedAt: { $exists: false },
+      deletedBy: { $exists: false },
+    };
+    const options = { fields: { _id: 1, createdAt: 1 } };
+    const docs = collection.find(query, options).fetch();
+    const result = docs.some(isStatusAwaitingReview);
+
+    return result;
+  }
+
+  _getReminderEmailData({ collection, reviewConfig, docType }) {
+    const receivers = this._getReceivers(collection, reviewConfig, docType);
+
+    if (!receivers.length) return false;
+
+    const prettyAnnualDate = getPrettyTzDate(reviewConfig.annualDate, this._timezone);
+    const title = `${getDocTypePlural(docType)}`;
+    const emailSubject = `The ${title} documents need a review`;
+    const emailText = `
+      The ${title} documents for "${this._organization.name}"
+      are scheduled for review on ${prettyAnnualDate}.
+      Please go to the ${capitalize(title)} screen to review them.
+    `;
+    const { serialNumber } = this._organization;
+
+    const buttonLabel = `Go to ${title}`;
+    const buttonUrl = getCollectionUrlByDocType(docType, serialNumber);
+
+    const templateData = {
+      organizationName: this._organization.name,
+      title: emailSubject,
+      text: emailText,
+      button: {
+        label: buttonLabel,
+        url: buttonUrl,
+      },
+    };
+
+    console.log(emailSubject, templateData);
+
+    return {
+      recipients: receivers,
+      emailSubject,
+      templateData,
+    };
+  }
+
+  _getReceivers() {
+    const ownerId = typeof this._organization.ownerId === 'function' &&
+      this._organization.ownerId();
+
+    return ownerId ? [ownerId] : [];
+  }
+
+  _sendReminders() {
+    this._reminders.forEach((reminder) => {
+      const reminderEmailData = this._getReminderEmailData(reminder);
+
+      if (!reminderEmailData) return;
+
+      new NotificationSender({
+        templateName: REMINDER_EMAIL_TEMPLATE,
+        ...reminderEmailData,
+      }).sendEmail();
+    });
+  }
+}
