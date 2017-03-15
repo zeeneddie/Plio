c960b075756987f241f408fd62d3f392f95ad0dc
Roman Valihura <roman.valihura@gmail.com>
2017-03-15 10:47:36 +0200

    store prev state in cache instead closure

--- app/imports/startup/server/cron/check-backgroud-alive.js
+++ app/imports/startup/server/cron/check-backgroud-alive.js
@@ -1,9 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
+import { tmpdir } from 'os';
+import { join } from 'path';
+import { readFileSync, writeFileSync, existsSync } from 'fs';
 import { PlioS3Logos } from '/imports/share/constants';
 import NotificationSender from '/imports/share/utils/NotificationSender';
 
-let prevIsCrashed = false;
 
 const BACKGROUND_APP_URL = Meteor.settings.backgroundApp.url;
 const CRASHED_MESSAGE = 'Background application was crashed with';
@@ -13,18 +15,23 @@ SyncedCron.add({
   name: 'Check alive background application',
 
   schedule(parser) {
-    return parser.text('every 10 minute');
+    return parser.text('every 10 minutes');
   },
 
   job() {
+    const prevStateFile = join(tmpdir(), 'background_prev_is_crashed');
+
     HTTP.call('GET', BACKGROUND_APP_URL, (err, response) => {
       const isCrashed = Boolean(err || response && response.statusCode !== 200);
 
-      console.log(`Background application status: ${isCrashed ? 'CRASHED' : 'RUNNING'} ${err} ${response}`);
-      console.log(`Previous background application status: ${prevIsCrashed ? 'CRASHED' : 'RUNNING'}`);
+      const prevIsCrashed = existsSync(prevStateFile)
+        ? JSON.parse(readFileSync(prevStateFile))
+        : !isCrashed;
+
       if (prevIsCrashed === isCrashed) return;
 
-      prevIsCrashed = isCrashed;
+      writeFileSync(prevStateFile, JSON.stringify(isCrashed));
+
       const emailTitle = `Background application status: ${isCrashed ? 'CRASHED' : 'RUNNING'}`;
       const secondaryText = isCrashed
         ? [CRASHED_MESSAGE, err].join(' ')
