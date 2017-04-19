1b23a1f922c8ba6e1f5865f150f9ede68385dd6a
volodymyrtymets <volodymyrtymets@gmail.com>
2017-04-19 13:56:45 +0100

    refactor tests

--- background-app/test/ReviewReminderSender.test.js
+++ background-app/test/ReviewReminderSender.test.js
@@ -7,58 +7,56 @@ import { Risks } from '/imports/share/collections/risks';
 import { Standards } from '/imports/share/collections/standards';
 
 describe('Review Reminder ', function () {
-  const organization1 = Organizations.findOne({ _id: 'KwKXz5RefrE5hjWJ2' });
-  const organization2 = Organizations.findOne({ _id: '8bcNPN8sKTtJFw4uy' });
-  const annualDate = new Date('04-04-2017');
+  const organization1 = Organizations.findOne({ _id: '8bcNPN8sKTtJFw4uy' });
+  const organization2 = Organizations.findOne({ _id: 'KwKXz5RefrE5hjWJ2' });
+  const annualDate = new Date('04-15-2017');
+  const todayDate = new Date('04-17-2017 05:00');
+  /**
+   * Show when need send Review Reminder notification
+   * deadline = annualDate + frequency;
+      annualDate     deadline - start       deadline     deadline + until
+   <--------*------------------*------------------*--------------*------->
+   ____________________________++++++$++++++++$+++++++++$+++++++++________
+                                interval  interval   interval
+   todayDay ->0000000000000000000000010000000010000000001000000000000000000
+                                    pass     pas       pass
+  **/
 
   const frequency = {
-    timeValue: 3,
+    timeValue: 3, // todayDate + timeValue = deadline;
     timeUnit: 'days'
   };
   const reminders = {
     start: {
-      timeValue: 2,
+      timeValue: 1, // deadline - timeValue = passDate
       timeUnit: 'days'
     },
     interval: {
-      timeValue: 1,
+      timeValue: 1, // - every day
       timeUnit: 'days'
     },
     until: {
-      timeValue: 3,
+      timeValue: 1, // deadline + timeValue = passDate
       timeUnit: 'days'
     }
   };
-  it('it should find time zones at 05:00', function () {
-    const todayDate = new Date('04-05-2017 05:00');
-    const timezones = getTimezones('05:00', todayDate);
-    assert.notEqual(timezones.length, 0);
-  });
 
-  it('it should not find time zones at 05:01', function () {
-    const todayDate = new Date('04-05-2017 05:01');
-    const timezones = getTimezones('05:00', todayDate);
-    assert.equal(timezones.length, 0);
-  });
-
-  it('it should find time zones from current date', function () {
-    const todayDate = new Date('04-05-2017 05:01');
+  it(`it should find time zones [Europe/London] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const todayMoment = moment(todayDate);
     const timezones = getTimezones(todayMoment.format('hh:mm'), todayDate);
-    assert.notEqual(timezones.length, 0);
+    assert.notEqual(timezones.includes('Europe/London'), false);
   });
 
-  it('it should find organization with time zone', function () {
-    const todayDate = new Date('04-05-2017 05:00');
+  it(`it should find organization with time zone  on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const timezones = getTimezones(moment(todayDate).format('hh:mm'), todayDate);
     const query = { timezone: { $in: timezones } };
+
     const organizationCount = Organizations.find(query).count();
     assert.notEqual(organizationCount, 0);
   });
 
-  it(`it should date pass for [${organization1.name}] on [04-05-2017]`, function () {
+  it(`it should date pass for [${organization1.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization1;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.standards.annualDate  = annualDate;
     organization.review.standards.frequency  = frequency;
@@ -70,25 +68,23 @@ describe('Review Reminder ', function () {
     assert.equal(result, true);
   });
 
-  it(`it should find Risks or Standards with status [AwaitingReview] for [${organization1.name}]`, function () {
+  it(`it should find Risks or Standards with status [AwaitingReview] for [${organization1.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization1;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.standards.annualDate  = annualDate;
     organization.review.standards.frequency  = frequency;
     organization.review.standards.reminders  = reminders;
 
-    const itHasRisks = new ReviewReminderSender(organization, todayDate).
-        _isHasStatusAwaitingReview(Risks, organization.review.risks);
+    // const itHasRisks = new ReviewReminderSender(organization, todayDate).
+    //     _isHasStatusAwaitingReview(Risks, organization.review.risks);
     const itHasStandards = new ReviewReminderSender(organization, todayDate).
     _isHasStatusAwaitingReview(Standards, organization.review.standards,);
 
-    assert.equal(itHasRisks || itHasStandards, true);
+    assert.equal(itHasStandards, true);
   });
 
-  it(`it should send Notification for [${organization1.name}] on [04-05-2017]`, function () {
+  it(`it should send Notification for [${organization1.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization1;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.risks.annualDate  = annualDate;
     organization.review.risks.frequency  = frequency;
@@ -99,9 +95,8 @@ describe('Review Reminder ', function () {
     assert.equal(isSend, true);
   });
 
-  it(`it should date pass for [${organization2.name}] on [04-05-2017]`, function () {
+  it(`it should date pass for [${organization2.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization2;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.standards.annualDate  = annualDate;
     organization.review.standards.frequency  = frequency;
@@ -113,9 +108,9 @@ describe('Review Reminder ', function () {
     assert.equal(result, true);
   });
 
-  it(`it should find Risks or Standards with status [AwaitingReview] for [${organization2.name}]`, function () {
+
+  it(`it should find Risks or Standards with status [AwaitingReview] for [${organization2.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization2;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.standards.annualDate  = annualDate;
     organization.review.standards.frequency  = frequency;
@@ -129,9 +124,9 @@ describe('Review Reminder ', function () {
     assert.equal(itHasRisks || itHasStandards, true);
   });
 
-  it(`it should send Notification for [${organization2.name}] on [04-05-2017]`, function () {
+
+  it(`it should send Notification for [${organization2.name}] on [${moment(todayDate).format('MM-DD-YYYY')}]`, function () {
     const organization = organization2;
-    const todayDate = new Date('04-05-2017');
 
     organization.review.risks.annualDate  = annualDate;
     organization.review.risks.frequency  = frequency;
@@ -142,4 +137,4 @@ describe('Review Reminder ', function () {
     assert.equal(isSend, true);
   });
 
-})
+});
