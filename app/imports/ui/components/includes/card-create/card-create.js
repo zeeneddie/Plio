3503bd5988a983443a2d6b1feb1bea85637a7c57
meyginix <meyginix@gmail.com>
2017-05-22 12:17:40 +0300

    I499: fixed modal opening after doc creation

--- app/imports/ui/components/includes/card-create/card-create.js
+++ app/imports/ui/components/includes/card-create/card-create.js
@@ -1,18 +1,20 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
- Template.Card_Create.viewmodel({
-   mixin: ['modal', 'collapsing'],
-   insert(method, { ...args } = {}, cb) {
-     this.modal().callMethod(method, { ...args }, (err, _id) => {
-       if (err) return;
+Template.Card_Create.viewmodel({
+  mixin: ['modal', 'collapsing'],
+  insert(method, { ...args } = {}, cb) {
+    this.modal().callMethod(method, { ...args }, (err, _id) => {
+      if (err) return;
 
-       this.modal().close();
+      const afterClose = () => {
+        Meteor.setTimeout(() => {
+          cb(_id, this.modal().open);
+          this.expandCollapsed(_id);
+        }, 400);
+      };
 
-       Meteor.setTimeout(() => {
-         cb(_id, this.modal().open);
-
-         this.expandCollapsed(_id);
-       }, 400);
-     });
-   }
- });
+      this.modal().close(afterClose);
+    });
+  },
+});
