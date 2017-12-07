e2f80fab68859e973bc57839050384a3d11099dc
RevelsGit <revels0k@yandex.ru>
2017-12-07 18:03:12 +0200

    dynamically import user directory edit card

--- app/imports/ui/components/userdirectory/card/card.js
+++ app/imports/ui/components/userdirectory/card/card.js
@@ -1,13 +1,13 @@
 import { Template } from 'meteor/templating';
-import { UserRolesNames } from '/imports/share/constants.js';
 import { UserProfileHelp } from '/imports/api/help-messages.js';
 
 Template.UserDirectory_Card_Read.viewmodel({
   share: 'search',
   mixin: ['user', 'organization', 'modal'],
-
-  openEditUserModal(e) {
+  async openEditUserModal(e) {
     e.preventDefault();
+
+    await import('../includes/edit');
 
     this.modal().open({
       template: 'UserDirectory_Card_Edit',
@@ -16,7 +16,6 @@ Template.UserDirectory_Card_Read.viewmodel({
       userId: this.user()._id,
     });
   },
-
   noSearchResults() {
     return this.searchText() && !this.searchResult().array().length;
   },
