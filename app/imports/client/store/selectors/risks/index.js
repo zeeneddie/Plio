0f869a9916bc6846a84833d8b800a67eee2a1d16
RevelsGit <revels0k@yandex.ru>
2017-12-14 16:20:53 +0200

    getSearchedFilteredSortedRisksByFilter selector, refactored risks lhs container

--- app/imports/client/store/selectors/risks/index.js
+++ app/imports/client/store/selectors/risks/index.js
@@ -2,3 +2,7 @@ export * from './state';
 export { default as getIsDeletedFilter } from './getIsDeletedFilter';
 export { default as getFilteredRisks } from './getFilteredRisks';
 export { default as getSearchedRisks } from './getSearchedRisks';
+export { default as getSortedRisksByFilter } from './getSortedRisksByFilter';
+export {
+  default as getSearchedFilteredSortedRisksByFilter,
+} from './getSearchedFilteredSortedRisksByFilter';
