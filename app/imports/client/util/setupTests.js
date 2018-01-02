567a3b7df212279c6f3c43f39e31e282a03fd157
RevelsGit <revels0k@yandex.ru>
2018-01-02 12:05:35 +0200

    DashboardStatsUnreadMessages component

--- app/imports/client/util/setupTests.js
+++ app/imports/client/util/setupTests.js
@@ -3,3 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 
 configure({ adapter: new Adapter() });
+
+global.requestAnimationFrame = cb => cb(0);
+global.window.cancelAnimationFrame = () => null;
--- app/imports/ui/react/dashboard/components/DashboardStatsUnreadMessages.js
+++ app/imports/ui/react/dashboard/components/DashboardStatsUnreadMessages.js
@@ -1,19 +1,63 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import pluralize from 'pluralize';
+import { map } from 'ramda';
 
-import { DashboardStats } from '../../components';
-import DashboardStatsUserList from './DashboardStatsUserList';
+import { DashboardStats, Button, Icon, IconLoading } from '../../components';
 
 export const DashboardUserStats = ({
   messages,
   count,
+  markAllAsRead,
+  hasItemsToLoad,
+  loadAll,
+  isReady,
+  hiddenUnreadMessagesNumber,
+  enableLimit,
 }) => !!messages.length && (
   <DashboardStats>
     <DashboardStats.Title>
       {count}
-      
+      <Button onClick={markAllAsRead}>
+        <Icon name="times-circle" title="Mark all messages as read" />
+      </Button>
     </DashboardStats.Title>
+    {map(({
+      url,
+      extension,
+      fullName,
+      text,
+      timeString,
+    }) => (
+      <a className="dashboard-stats-message" href={url}>
+        {extension ? (
+          <Icon name={`file-${extension}-o`} />
+        ) : (
+          <Icon name="comment" />
+        )}
+        <strong>{fullName}:</strong>
+        {text}
+        <span className="text-muted">{timeString} ago</span>
+      </a>
+    ), messages)}
+    {hasItemsToLoad ? (
+      <Button color="secondary" onClick={loadAll}>
+        View all items
+        {isReady ? (
+          <span>({hiddenUnreadMessagesNumber} more)</span>
+        ) : (
+          <IconLoading margin="bottom" />
+        )}
+      </Button>
+    ) : enableLimit && (
+      <Button color="secondary" onClick={() => enableLimit(true)}>
+        <span>Hide</span>
+        {isReady ? (
+          <span>({hiddenUnreadMessagesNumber} items)</span>
+        ) : (
+          <IconLoading margin="bottom" />
+        )}
+      </Button>
+    )}
   </DashboardStats>
 );
 
