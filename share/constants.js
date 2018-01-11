e6313bc56268963644d33d56521a654ccab6d656
RevelsGit <revels0k@yandex.ru>
2018-01-11 10:50:26 +0200

    goals collection and schema for it

--- share/constants.js
+++ share/constants.js
@@ -238,7 +238,6 @@ export const ProblemMagnitudes = {
   CRITICAL: 'critical',
 };
 
-
 export const ProblemIndexes = {
   REPORTED: 1,
   AWAITING_ANALYSIS: 2,
@@ -364,6 +363,9 @@ export const StringLimits = {
   title: {
     min: 1,
     max: 80,
+  },
+  description: {
+    max: 240,
   },
   url: {
     min: 1,
@@ -712,4 +714,22 @@ export const WorkspaceDefaults = {
   [WorkspaceDefaultsTypes.DISPLAY_USERS]: 5,
   [WorkspaceDefaultsTypes.DISPLAY_MESSAGES]: 1,
   [WorkspaceDefaultsTypes.DISPLAY_ACTIONS]: 4,
+};
+
+export const GoalPriorities = { ...ProblemMagnitudes };
+
+export const GoalStatuses = {
+  AWAITING_COMPLETION: 1,
+  OVERDUE: 2,
+  COMPLETED: 3,
+};
+
+export const GoalStatusesNames = {
+  [GoalStatuses.AWAITING_COMPLETION]: 'Open - awaiting completion',
+  [GoalStatuses.OVERDUE]: 'Open - overdue',
+  [GoalStatuses.COMPLETED]: 'Closed - marked as complete',
+};
+
+export const GoalColors = {
+  // ?
 };
