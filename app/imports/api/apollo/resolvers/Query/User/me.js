7faafd8b6fbf0a639ef305712cb05828abde0b6e
RevelsGit <revels0k@yandex.ru>
2018-01-16 16:51:35 +0200

    wat

--- app/imports/api/apollo/resolvers/Query/User/me.js
+++ app/imports/api/apollo/resolvers/Query/User/me.js
@@ -1,14 +1,10 @@
 import { Meteor } from 'meteor/meteor';
 
-export default function (root, args, context) {
+export default function (root, args, { userId, user }) {
   // if the user is not logged in throw an error
-  if (!context.userId) {
+  if (!userId) {
     throw new Meteor.Error(403, 'Unknown User (not logged in)');
   }
 
-  // Only return the current user, for security
-  if (context._id === args.id) {
-    return context.user;
-  }
-  return context.user;
+  return user;
 }
