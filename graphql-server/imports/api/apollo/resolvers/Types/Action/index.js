14024643811c5ea641a09971fc262720c4e5e1cb
RevelsGit <revels0k@yandex.ru>
2018-01-17 19:44:55 +0200

    action schema resolvers

--- graphql-server/imports/api/apollo/resolvers/Types/Action/index.js
+++ graphql-server/imports/api/apollo/resolvers/Types/Action/index.js
@@ -1,5 +1,16 @@
-import { loadUserById } from 'plio-util';
-import { createdBy, updatedBy, completedBy } from 'plio-util/dist/lenses';
+import { loadUserById, loadUsersById } from 'plio-util';
+import {
+  createdBy,
+  updatedBy,
+  completedBy,
+  completionAssignedBy,
+  verificationAssignedBy,
+  deletedBy,
+  toBeCompletedBy,
+  toBeVerifiedBy,
+  verifiedBy,
+  notify,
+} from 'plio-util/dist/lenses';
 import { view } from 'ramda';
 
 export default {
@@ -7,5 +18,12 @@ export default {
     createdBy: loadUserById(view(createdBy)),
     updatedBy: loadUserById(view(updatedBy)),
     completedBy: loadUserById(view(completedBy)),
+    completionAssignedBy: loadUserById(view(completionAssignedBy)),
+    verificationAssignedBy: loadUserById(view(verificationAssignedBy)),
+    deletedBy: loadUserById(view(deletedBy)),
+    toBeCompletedBy: loadUserById(view(toBeCompletedBy)),
+    toBeVerifiedBy: loadUserById(view(toBeVerifiedBy)),
+    verifiedBy: loadUserById(view(verifiedBy)),
+    notify: loadUsersById(view(notify)),
   },
 };
