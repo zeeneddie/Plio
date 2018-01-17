c462ff1c51c8688d77c5b6e16eff00f4aa7a44eb
RevelsGit <revels0k@yandex.ru>
2018-01-17 17:08:40 +0200

    action schema

--- graphql-server/imports/api/apollo/schema/Types/index.js
+++ graphql-server/imports/api/apollo/schema/Types/index.js
@@ -1,9 +1,11 @@
 import User from './User.graphql';
 import File from './File.graphql';
 import Organization from './Organization.graphql';
+import Action from './Action';
 
 export default [
   User,
   File,
   Organization,
+  Action,
 ];
