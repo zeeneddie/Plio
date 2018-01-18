71f0b247e37b6f282cb02cf49c7b09f0c4ae19b9
RevelsGit <revels0k@yandex.ru>
2018-01-18 12:28:21 +0200

    revamped file schema

--- graphql-server/imports/api/apollo/resolvers/Types/File/index.js
+++ graphql-server/imports/api/apollo/resolvers/Types/File/index.js
@@ -1,6 +1,16 @@
+import { loadOrganizationById, loadUserById, lenses } from 'plio-util';
+import { view } from 'ramda';
+
+const {
+  organizationId,
+  createdBy,
+  updatedBy,
+} = lenses;
+
 export default {
   File: {
-    organization: async ({ organizationId }, _, { collections: { Organizations } }) =>
-      Organizations.findOne({ _id: organizationId }),
+    createdBy: loadUserById(view(createdBy)),
+    updatedBy: loadUserById(view(updatedBy)),
+    organization: loadOrganizationById(view(organizationId)),
   },
 };
