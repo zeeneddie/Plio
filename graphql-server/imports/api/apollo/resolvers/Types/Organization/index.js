220f0d12701637a8f9faa6fdeb058afe71d118dc
RevelsGit <revels0k@yandex.ru>
2018-01-18 12:24:54 +0200

    revamped organization schema

--- graphql-server/imports/api/apollo/resolvers/Types/Organization/index.js
+++ graphql-server/imports/api/apollo/resolvers/Types/Organization/index.js
@@ -1,8 +1,17 @@
-import { loadUserById } from 'plio-util';
-import { userId } from 'plio-util/dist/lenses';
+import { loadUserById, lenses } from 'plio-util';
 import { view } from 'ramda';
 
+const {
+  userId,
+  createdBy,
+  updatedBy,
+} = lenses;
+
 export default {
+  Organization: {
+    createdBy: loadUserById(view(createdBy)),
+    updatedBy: loadUserById(view(updatedBy)),
+  },
   OrganizationUser: {
     user: loadUserById(view(userId)),
   },
