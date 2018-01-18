4a6a67e12cf74d38ec34be4b5c834823febeb7aa
RevelsGit <revels0k@yandex.ru>
2018-01-18 13:02:57 +0200

    Analysis sgraphql schema

--- graphql-server/imports/api/apollo/schema/Types/index.js
+++ graphql-server/imports/api/apollo/schema/Types/index.js
@@ -2,10 +2,14 @@ import User from './User.graphql';
 import File from './File.graphql';
 import Organization from './Organization.graphql';
 import Action from './Action.graphql';
+import Risk from './Risk.graphql';
+import Analysis from './Analysis.graphql';
 
 export default [
   User,
   File,
   Organization,
   Action,
+  Risk,
+  Analysis,
 ];
