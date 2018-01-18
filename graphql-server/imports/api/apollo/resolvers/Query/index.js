eda70724be56015fb3ea58bd1937072723ddc282
RevelsGit <revels0k@yandex.ru>
2018-01-18 13:28:56 +0200

    sample risks query type

--- graphql-server/imports/api/apollo/resolvers/Query/index.js
+++ graphql-server/imports/api/apollo/resolvers/Query/index.js
@@ -5,4 +5,5 @@ export default {
   // TEMP
   organizations: (r, a, { collections: { Organizations } }) => Organizations.find().fetch(),
   actions: (r, a, { collections: { Actions } }) => Actions.find().fetch(),
+  risks: (r, a, { collections: { Risks } }) => Risks.find().fetch(),
 };
