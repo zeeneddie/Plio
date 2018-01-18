2fb367a1c19e4ba37fb8d3cb3346ee62015b96ac
RevelsGit <revels0k@yandex.ru>
2018-01-18 18:17:36 +0200

    risk, lesson, action loaders

---
+++ graphql-server/imports/api/apollo/loaders/Risk/byGoalIds.js
@@ -0,0 +1,6 @@
+import DataLoader from 'dataloader';
+
+import { Risks } from '../../../../share/collections';
+
+export default new DataLoader(async ids =>
+  Promise.all(ids.map(id => Risks.find({ goalIds: id }).fetch())));
