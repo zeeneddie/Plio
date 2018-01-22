deed5df262dba4887c6539ee932747649eb92a60
RevelsGit <revels0k@yandex.ru>
2018-01-22 11:46:59 +0200

    basic example with some issues

--- app/imports/ui/react/dashboard/components/DashboardGoals.js
+++ app/imports/ui/react/dashboard/components/DashboardGoals.js
@@ -2,22 +2,76 @@
 import React from 'react';
 import {
   VictoryChart,
-  // VictorySharedEvents,
-  // VictoryZoomContainer,
+  VictoryZoomContainer,
   VictoryAxis,
-  // VictoryClipContainer,
   VictoryLine,
+  VictoryLabel,
+  VictoryScatter,
+  VictoryTooltip,
 } from 'victory';
 
-const DashboardGoals = ({ data: { loading, goals } }) => !loading && (
+const fontFamily = '"Roboto", "Helvetica Neue", Helvetica, sans-serif';
+
+const getLineData = ({
+  startDate,
+  endDate,
+  title,
+  milestones = [],
+}, index) => [
+  { x: new Date(startDate), y: index, label: title },
+  { x: new Date(endDate), y: index },
+  ...milestones.map(({ completionTargetDate }, y) => ({
+    y,
+    x: new Date(completionTargetDate),
+  })),
+];
+
+const getScatterData = ({
+  title,
+  startDate,
+  endDate,
+  color,
+  milestones = [],
+}, index) => [
+  {
+    x: new Date(startDate),
+    y: index,
+    label: `${title} \n start date: \n ${new Date(startDate).toDateString()}`,
+    symbol: 'circle',
+    strokeWidth: 7,
+    fill: color,
+  },
+  {
+    x: new Date(endDate),
+    y: index,
+    label: `${title} \n end date: \n ${new Date(endDate).toDateString()}`,
+    symbol: 'circle',
+    strokeWidth: 7,
+    fill: color,
+  },
+  ...milestones.map(({ completionTargetDate, title: milestoneTitle }) => ({
+    x: new Date(completionTargetDate),
+    y: index,
+    label: milestoneTitle,
+    symbol: 'square',
+    strokeWidth: 3,
+    size: 5,
+    fill: color,
+  })),
+];
+
+const DashboardGoals = ({
+  data: { loading, goals },
+  zoomDomain,
+  onZoom,
+  onLineTap,
+  onScatterTap,
+}) => !loading && (
   <VictoryChart
-    standalone
     width={1140}
-    height={200}
+    height={400}
     scale={{ x: 'time' }}
-    domainPadding={{
-      x: [20, 20],
-    }}
+    domainPadding={{ x: [20, 20] }}
     domain={{
       x: [
         new Date('01 January 2018'),
@@ -28,7 +82,13 @@ const DashboardGoals = ({ data: { loading, goals } }) => !loading && (
         goals.length,
       ],
     }}
-    // containerComponent={<VictoryZoomContainer dimension="x" />}
+    containerComponent={(
+      <VictoryZoomContainer
+        dimension="x"
+        zoomDomain={zoomDomain}
+        onDomainChange={onZoom}
+      />
+    )}
     padding={{
       top: 0,
       left: 0,
@@ -36,23 +96,77 @@ const DashboardGoals = ({ data: { loading, goals } }) => !loading && (
       bottom: 0,
     }}
   >
-    <VictoryAxis />
-    <VictoryLine
-      data={[{
-        x: new Date(),
-        y: 0,
-        label: 'today',
-      }, {
-        x: new Date(),
-        y: 200,
-      }]}
+    <VictoryAxis
+      style={{
+        axis: {
+          stroke: 'none',
+        },
+        tickLabels: {
+          angle: 0,
+          padding: 30,
+          border: 1,
+          fontFamily,
+        },
+      }}
     />
-    {/* {goals.map(({ _id }) => (
+    <VictoryLine
+      style={{
+        data: { stroke: '#888', strokeWidth: 1, fontFamily },
+        labels: { fill: '#888' },
+      }}
+      data={[
+        { x: new Date(), y: 0, label: 'today' },
+        { x: new Date(), y: 400 },
+      ]}
+      labelComponent={<VictoryLabel dy={35} />}
+    />
+    {goals.map((goal, index) => (
       <VictoryLine
-        key={_id}
+        key={index}
         animate={{ duration: 500 }}
+        style={{
+          data: {
+            stroke: goal.color,
+            strokeWidth: 4,
+            cursor: 'pointer',
+          },
+          labels: {
+            fontFamily,
+            fill: '#888',
+            fontWeight: 'lighter',
+            textAnchor: 'start',
+          },
+        }}
+        events={[{
+          target: 'data',
+          eventHandlers: {
+            onClick: e => onLineTap(e, goal),
+          },
+        }]}
+        data={getLineData(goal, index)}
+        labelComponent={<VictoryLabel x={450} />}
       />
-    ))} */}
+    ))}
+    {goals.map((goal, index) => (
+      <VictoryScatter
+        key={index}
+        style={{
+          data: {
+            stroke: goal.color,
+            cursor: 'pointer',
+          },
+          labels: { fontFamily },
+        }}
+        events={[{
+          target: 'data',
+          eventHandlers: {
+            onClick: onScatterTap,
+          },
+        }]}
+        data={getScatterData(goal, index)}
+        labelComponent={<VictoryTooltip />}
+      />
+    ))}
   </VictoryChart>
 );
 
