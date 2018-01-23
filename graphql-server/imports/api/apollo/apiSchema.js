0fea99815486ebc6dea7327d8f17dde9eda56c21
RevelsGit <revels0k@yandex.ru>
2018-01-23 16:35:19 +0200

    add mocks to schema only in dev and if MOCK_SCHEMA env variable is set

--- graphql-server/imports/api/apollo/apiSchema.js
+++ graphql-server/imports/api/apollo/apiSchema.js
@@ -1,4 +1,4 @@
-import { makeExecutableSchema } from 'graphql-tools';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import { loadSchema, getSchema } from 'graphql-loader';
 
 import typeDefs from './schema';
@@ -7,5 +7,15 @@ import resolvers from './resolvers';
 loadSchema({ typeDefs, resolvers });
 
 const schema = makeExecutableSchema(getSchema());
+
+if (process.env.NODE_ENV !== 'production' && parseInt(process.env.MOCK_SCHEMA, 10) === 1) {
+  const mocks = require('./schema/mocks').default;
+
+  addMockFunctionsToSchema({
+    schema,
+    mocks,
+    preserveResolvers: false,
+  });
+}
 
 export default schema;
