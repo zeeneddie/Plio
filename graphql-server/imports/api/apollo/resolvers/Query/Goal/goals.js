f5987d1444474d025c078a3229e2cfc29036387b
RevelsGit <revels0k@yandex.ru>
2018-01-23 13:13:15 +0200

    return meta info in goals query

--- graphql-server/imports/api/apollo/resolvers/Query/Goal/goals.js
+++ graphql-server/imports/api/apollo/resolvers/Query/Goal/goals.js
@@ -1,3 +1,12 @@
 // needs protection
-export default async (rootValue, { organizationId, limit }, { collections: { Goals } }) =>
-  Goals.find({ organizationId }, { limit }).fetch();
+export default async (rootValue, { organizationId, limit }, { collections: { Goals } }) => {
+  const query = { organizationId };
+  const options = { limit };
+  const goals = Goals.find(query, options).fetch();
+  const totalCount = Goals.find(query).count();
+
+  return {
+    totalCount,
+    goals,
+  };
+};
