38e31ef031ff19da725e11f2e58f54c5e08e6a2a
RevelsGit <revels0k@yandex.ru>
2018-01-23 16:38:15 +0200

    import addMockFunctionsToSchema in the body of condition

--- graphql-server/imports/api/apollo/apiSchema.js
+++ graphql-server/imports/api/apollo/apiSchema.js
@@ -1,4 +1,4 @@
-import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { makeExecutableSchema } from 'graphql-tools';
 import { loadSchema, getSchema } from 'graphql-loader';
 
 import typeDefs from './schema';
@@ -9,6 +9,7 @@ loadSchema({ typeDefs, resolvers });
 const schema = makeExecutableSchema(getSchema());
 
 if (process.env.NODE_ENV !== 'production' && parseInt(process.env.MOCK_SCHEMA, 10) === 1) {
+  const { addMockFunctionsToSchema } = require('graphql-tools');
   const mocks = require('./schema/mocks').default;
 
   addMockFunctionsToSchema({
