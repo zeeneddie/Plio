51f558828af9083b327aff81d37e02e75cde4a56
RevelsGit <revels0k@yandex.ru>
2018-01-23 15:12:07 +0200

    query for not deleted goals only by default

--- graphql-server/imports/api/apollo/resolvers/Query/Goal/goals.js
+++ graphql-server/imports/api/apollo/resolvers/Query/Goal/goals.js
@@ -1,9 +1,24 @@
 // needs protection
-export default async (rootValue, { organizationId, limit }, { collections: { Goals } }) => {
-  const query = { organizationId };
+export default async (
+  rootValue,
+  {
+    organizationId,
+    limit,
+    withDeleted,
+  },
+  {
+    collections: {
+      Goals,
+    },
+  },
+) => {
+  const query = { organizationId, isDeleted: false };
   const options = { limit };
-  const goals = Goals.find(query, options).fetch();
-  const totalCount = Goals.find(query).count();
+  const totalCount = await Goals.find(query).count();
+
+  if (withDeleted) delete query.isDeleted;
+
+  const goals = await Goals.find(query, options).fetch();
 
   return {
     totalCount,
