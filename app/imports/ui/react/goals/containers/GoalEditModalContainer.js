d2e06559dede1581342563388d6c3ea11cddaeff
RevelsGit <revels0k@yandex.ru>
2018-02-05 18:57:38 +0200

    Goal edit modal container

--- app/imports/ui/react/goals/containers/GoalEditModalContainer.js
+++ app/imports/ui/react/goals/containers/GoalEditModalContainer.js
@@ -1,6 +1,30 @@
-import { namedCompose } from '../../helpers';
+import connectUI from 'redux-ui';
+import { onlyUpdateForKeys, branch, renderNothing } from 'recompose';
+import { identity, path } from 'ramda';
+
+import { namedCompose, withFragment } from '../../helpers';
 import GoalEditModal from '../components/GoalEditModal';
+import { Fragment } from '../../../../client/graphql';
 
 export default namedCompose('GoalEditModalContainer')(
-
+  connectUI(),
+  branch(
+    path(['ui', 'activeGoal']),
+    identity,
+    renderNothing,
+  ),
+  withFragment(Fragment.DASHBOARD_GOAL, {
+    name: 'goal',
+    getFragmentId: ({ ui: { activeGoal } }) => `Goal:${activeGoal}`,
+    props: ({
+      goal,
+      isOpen,
+      toggle,
+    }) => ({
+      isOpen,
+      toggle,
+      goal,
+    }),
+  }),
+  onlyUpdateForKeys(['isOpen', 'goal']),
 )(GoalEditModal);
