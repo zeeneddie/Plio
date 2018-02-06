acbfc8c8bcf43effb66feeebe3a82b846cb4a49c
RevelsGit <revels0k@yandex.ru>
2018-02-06 16:49:34 +0200

    goal update description mutation

---
+++ graphql-server/imports/api/apollo/resolvers/Mutation/Goal/updateGoalDescription.js
@@ -0,0 +1,20 @@
+import { applyMiddleware } from 'plio-util';
+import {
+  checkLoggedIn,
+  flattenInput,
+  checkGoalAccess,
+} from '../../../../../share/middleware';
+
+export const resolver = async (
+  root,
+  args,
+  {
+    services: { GoalService },
+  },
+) => GoalService.updateTitle(args);
+
+export default applyMiddleware(
+  checkLoggedIn(),
+  flattenInput(),
+  checkGoalAccess(),
+)(resolver);
