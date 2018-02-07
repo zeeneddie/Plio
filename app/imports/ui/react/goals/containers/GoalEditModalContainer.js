978f54feb95f25765f84a250591714637eb3d6b1
RevelsGit <revels0k@yandex.ru>
2018-02-07 20:25:52 +0200

    WIP: delete goal from edit modal

--- app/imports/ui/react/goals/containers/GoalEditModalContainer.js
+++ app/imports/ui/react/goals/containers/GoalEditModalContainer.js
@@ -1,12 +1,28 @@
 import connectUI from 'redux-ui';
 import { onlyUpdateForKeys, branch, renderNothing } from 'recompose';
-import { identity, path } from 'ramda';
+import { identity, path, over, dropLast, dec, compose } from 'ramda';
 import { graphql } from 'react-apollo';
-import { transformGoal } from 'plio-util';
+import { transformGoal, lenses } from 'plio-util';
+import gql from 'graphql-tag';
 
 import { namedCompose } from '../../helpers';
 import GoalEditModal from '../components/GoalEditModal';
 import { Query } from '../../../../client/graphql';
+
+const deleteGoal = compose(
+  over(lenses.goals.totalCount, dec),
+  over(lenses.goals.goals, dropLast(1)),
+);
+
+const DELETE_GOAL = gql`
+  mutation deleteGoal($input: DeleteGoalInput!) {
+    deleteGoal(input: $input) {
+      goal {
+        _id
+      }
+    }
+  }
+`;
 
 export default namedCompose('GoalEditModalContainer')(
   connectUI(),
@@ -41,6 +57,39 @@ export default namedCompose('GoalEditModalContainer')(
       toggle,
       organizationId,
       goal: goal ? transformGoal(goal) : null,
+    }),
+  }),
+  graphql(DELETE_GOAL, {
+    props: ({
+      mutate,
+      ownProps: {
+        goal,
+        organizationId,
+        ...props
+      },
+    }) => ({
+      goal,
+      organizationId,
+      ...props,
+      onDelete: () => mutate({
+        variables: {
+          input: {
+            _id: goal._id,
+          },
+        },
+        update: (proxy) => {
+          const data = proxy.readQuery({
+            query: Query.DASHBOARD_GOALS,
+            variables: { organizationId },
+          });
+
+          return proxy.writeQuery({
+            query: Query.DASHBOARD_GOALS,
+            variables: { organizationId },
+            data: deleteGoal(data),
+          });
+        },
+      }),
     }),
   }),
   onlyUpdateForKeys(['isOpen', 'goal', 'organizationId']),
