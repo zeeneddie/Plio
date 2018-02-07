ece343981684740a916d8bc22e12345905df9b7d
RevelsGit <revels0k@yandex.ru>
2018-02-07 11:03:57 +0200

    editable goal start date, end date, priority and color fields

--- app/imports/ui/react/goals/containers/GoalEditContainer.js
+++ app/imports/ui/react/goals/containers/GoalEditContainer.js
@@ -1,8 +1,8 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { flattenProp, mapProps } from 'recompose';
-import { lenses } from 'plio-util';
-import { view, curry, compose, objOf } from 'ramda';
+import { flattenProp } from 'recompose';
+import { lenses, getTargetValue, toDate } from 'plio-util';
+import { view, curry, compose, objOf, toUpper } from 'ramda';
 
 import { namedCompose } from '../../helpers';
 import GoalEdit from '../components/GoalEdit';
@@ -92,9 +92,57 @@ const UPDATE_GOAL_OWNER = gql`
   }
 `;
 
-const getUpdateTitleInputArgs = compose(objOf('title'), view(lenses.target.value));
-const getUpdateDescriptionInputArgs = compose(objOf('description'), view(lenses.target.value));
+const UPDATE_GOAL_START_DATE = gql`
+  mutation updateGoalStartDate($input: UpdateGoalStartDateInput!) {
+    updateGoalStartDate(input: $input) {
+      goal {
+        _id
+        startDate
+      }
+    }
+  }
+`;
+
+const UPDATE_GOAL_END_DATE = gql`
+  mutation updateGoalEndDate($input: UpdateGoalEndDateInput!) {
+    updateGoalEndDate(input: $input) {
+      goal {
+        _id
+        endDate
+      }
+    }
+  }
+`;
+
+const UPDATE_GOAL_PRIORITY = gql`
+  mutation updateGoalPriority($input: UpdateGoalPriorityInput!) {
+    updateGoalPriority(input: $input) {
+      goal {
+        _id
+        priority
+      }
+    }
+  }
+`;
+
+const UPDATE_GOAL_COLOR = gql`
+  mutation updateGoalColor($input: UpdateGoalColorInput!) {
+    updateGoalColor(input: $input) {
+      goal {
+        _id
+        color
+      }
+    }
+  }
+`;
+
+const getUpdateTitleInputArgs = compose(objOf('title'), getTargetValue);
+const getUpdateDescriptionInputArgs = compose(objOf('description'), getTargetValue);
 const getUpdateOwnerInputArgs = compose(objOf('ownerId'), (_, { value }) => value);
+const getUpdateStartDateInputArgs = compose(objOf('startDate'), toDate);
+const getUpdateEndDateInputArgs = compose(objOf('endDate'), toDate);
+const getUpdatePriorityInputArgs = compose(objOf('priority'), getTargetValue);
+const getUpdateColorInputArgs = compose(objOf('color'), toUpper, view(lenses.hex));
 
 export default namedCompose('GoalEditContainer')(
   graphql(UPDATE_GOAL_TITLE, {
@@ -115,12 +163,29 @@ export default namedCompose('GoalEditContainer')(
       mutation: 'updateGoalOwner',
     }),
   }),
-  mapProps(props => ({
-    ...props,
-    onChangeStartDate: () => null,
-    onChangeEndDate: () => null,
-    onChangePriority: () => null,
-    onChangeColor: () => null,
-  })),
+  graphql(UPDATE_GOAL_START_DATE, {
+    props: props(getUpdateStartDateInputArgs, {
+      handler: 'onChangeStartDate',
+      mutation: 'updateGoalStartDate',
+    }),
+  }),
+  graphql(UPDATE_GOAL_END_DATE, {
+    props: props(getUpdateEndDateInputArgs, {
+      handler: 'onChangeEndDate',
+      mutation: 'updateGoalEndDate',
+    }),
+  }),
+  graphql(UPDATE_GOAL_PRIORITY, {
+    props: props(getUpdatePriorityInputArgs, {
+      handler: 'onChangePriority',
+      mutation: 'updateGoalPriority',
+    }),
+  }),
+  graphql(UPDATE_GOAL_COLOR, {
+    props: props(getUpdateColorInputArgs, {
+      handler: 'onChangeColor',
+      mutation: 'updateGoalColor',
+    }),
+  }),
   flattenProp('goal'),
 )(GoalEdit);
