e07fb8ed0f2ff09382022c93001380bbff8f8a63
RevelsGit <revels0k@yandex.ru>
2018-02-13 14:31:43 +0200

    refactored existing middleware to be more flexible

--- share/middleware/auth/checkOrgMembership.js
+++ share/middleware/auth/checkOrgMembership.js
@@ -1,15 +1,14 @@
-import { Meteor } from 'meteor/meteor';
-import { view } from 'ramda';
-import { lenses } from 'plio-util';
 
 import { isOrgMember } from '../../checkers';
 import Errors from '../../errors';
 
-export default (getOrgId = view(lenses.organizationId)) => async (next, root, args, context) => {
-  const organizationId = getOrgId(args, context);
+export default (
+  getOrgId = (root, args) => args.organizationId,
+) => async (next, root, args, context) => {
+  const organizationId = getOrgId(root, args, context);
 
-  if (!await isOrgMember(organizationId, view(lenses.userId, context))) {
-    throw new Meteor.Error(403, Errors.NOT_ORG_MEMBER);
+  if (!await isOrgMember(organizationId, context.userId)) {
+    throw new Error(Errors.NOT_ORG_MEMBER);
   }
 
   return next(root, args, context);
