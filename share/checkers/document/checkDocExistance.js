ed59ee3281f71c7c5b75ceb7dc85c50518b012d1
RevelsGit <revels0k@yandex.ru>
2018-02-13 14:55:56 +0200

    checkDocExistance tests

--- share/checkers/document/checkDocExistance.js
+++ share/checkers/document/checkDocExistance.js
@@ -4,7 +4,7 @@ import Errors from '../../errors';
 export default async (query, collection) => {
   const doc = await collection.findOne(query);
 
-  if (!doc) throw new Meteor.Error(404, Errors.NOT_FOUND);
+  if (!doc) throw new Error(Errors.NOT_FOUND);
 
   return doc;
 };
