79503b6959a4afb8591dc5cf3e337a05174e7028
Zinchuk Serhiy <serhiy91z@gmail.com>
2018-03-19 10:01:49 +0200

    I823 user graphql type roles (#829)
    * User roles resolver

--- graphql-server/imports/api/apollo/resolvers/Types/User/index.js
+++ graphql-server/imports/api/apollo/resolvers/Types/User/index.js
@@ -3,6 +3,7 @@ export default {
     email(user) {
       return user.emails[0].address;
     },
+    roles: (user, { organizationId }) => user.roles[organizationId],
   },
   UserProfile: {
     fullName: ({ firstName, lastName }) => `${firstName} ${lastName}`,
