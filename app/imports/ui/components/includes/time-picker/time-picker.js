1cc7cefc2f2e87af00aea45bcd349d6bfd6c0820
Dmytro Artamonov <revels0k@gmail.com>
2018-05-03 16:47:47 +0300

    fixed workflow reminders input not receiving focus when clicking on arrow in firefox (#995)

--- app/imports/ui/components/includes/time-picker/time-picker.js
+++ app/imports/ui/components/includes/time-picker/time-picker.js
@@ -1,12 +1,14 @@
-import { TimeUnits } from '/imports/share/constants.js';
+import { Template } from 'meteor/templating';
+import { and } from 'ramda';
 
+import { TimeUnits } from '../../../../share/constants';
 
 Template.TimePicker.viewmodel({
   mixin: 'callWithFocusCheck',
   timeValue: '',
   timeUnit: '',
   timeUnits() {
-    return _.values(TimeUnits);
+    return Object.values(TimeUnits);
   },
   isSelectedUnit(timeUnit) {
     return this.timeUnit() === timeUnit;
@@ -20,15 +22,18 @@ Template.TimePicker.viewmodel({
     const savedTimeUnit = context.timeUnit;
     const timeUnit = this.timeUnit();
 
-    return _.every([
+    return and(
       timeValue && timeUnit,
       (savedTimeValue !== timeValue) || (savedTimeUnit !== timeUnit),
-    ]);
+    );
   },
-  onFocusOut(e) {
+  onBlur(e) {
     if (this.isChanged()) {
       this.callWithFocusCheck(e, () => this.onChange(this));
     }
+  },
+  onMouseDown(e) {
+    e.target.focus();
   },
   updateTimeUnit(timeUnit) {
     this.timeUnit(timeUnit);
