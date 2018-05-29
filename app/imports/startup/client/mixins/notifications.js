bfc13a15cba8d3093d22196c4b627413a28e895e
Dmytro Artamonov <revels0k@gmail.com>
2018-05-29 17:19:12 +0300

    increased notification dissapearance timeout (#1141)

--- app/imports/startup/client/mixins/notifications.js
+++ app/imports/startup/client/mixins/notifications.js
@@ -6,7 +6,14 @@ export default {
   // _id is used as notification tag if there's no tag argument passed
   // Only title is required
   sendNotification({
-    _id, title, body, tag, icon, url, silent = true, timeout = 4000,
+    _id,
+    title,
+    body,
+    tag,
+    icon,
+    url,
+    silent = true,
+    timeout = 6000,
   }) {
     const notification = new Notification(title, {
       body,
