4a3f60560a977fedc8becaa18e63752bc9fb09e1
Dmytro Artamonov <revels0k@gmail.com>
2018-07-21 02:43:43 +0300

    added BaseEntity, PlioEntity and CanvasEntity types to graphql schema (#1218)

---
+++ graphql-server/imports/api/apollo/schema/Interface/index.js
@@ -0,0 +1,9 @@
+import BaseEntity from './BaseEntity.graphql';
+import CanvasEntity from './CanvasEntity.graphql';
+import PlioEntity from './PlioEntity.graphql';
+
+export default [
+  BaseEntity,
+  CanvasEntity,
+  PlioEntity,
+];
--- graphql-server/imports/api/apollo/schema/index.js
+++ graphql-server/imports/api/apollo/schema/index.js
@@ -2,10 +2,12 @@ import Query from './Query';
 import Mutation from './Mutation';
 import Types from './Types';
 import Scalars from './Scalars';
+import Interface from './Interface';
 
 export default [
   ...Query,
   ...Mutation,
   ...Types,
   ...Scalars,
+  ...Interface,
 ];
