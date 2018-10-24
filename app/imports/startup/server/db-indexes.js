ee979b0b05143510b1b70b3de5cae33b9356d620
Ivan Seniv <seniv97@gmail.com>
2018-10-24 16:37:16 +0300

    removed all "single" indexes from db-indexes.js file

--- app/imports/startup/server/db-indexes.js
+++ app/imports/startup/server/db-indexes.js
@@ -1,114 +1,30 @@
 import {
-  RiskTypes,
   Notifications,
   AuditLogs,
   Discussions,
   WorkItems,
   Actions,
   Messages,
-  Occurrences,
   NonConformities,
   Standards,
-  StandardTypes,
-  StandardsBookSections,
   Organizations,
-  LessonsLearned,
-  Departments,
   Risks,
   Goals,
-  Milestones,
-  Channels,
-  CustomerRelationships,
-  CostLines,
-  CustomerSegments,
-  KeyActivities,
-  KeyPartners,
-  KeyResources,
-  RevenueStreams,
-  ValuePropositions,
-  CanvasSettings,
-  Benefits,
-  Features,
-  Needs,
-  Wants,
   Relations,
 } from '../../share/collections';
 
-
-// indexes for Departments
-Departments._ensureIndex({
-  organizationId: 1,
-});
-
-
-// indexes for LessonsLearned
-LessonsLearned._ensureIndex({
-  organizationId: 1,
-});
-
-LessonsLearned._ensureIndex({
-  documentId: 1,
-});
-
+/*
+  All "Single" indexes created in collections Schemas https://github.com/aldeed/meteor-schema-index
+  But meteor-schema-index can't create COMPOUND indexes, so we should create them here.
+*/
 
 // indexes for Organizations
 Organizations._ensureIndex({
   serialNumber: 1,
-});
-
-Organizations._ensureIndex({
-  name: 1,
-}, {
-  unique: true,
-});
-
-Organizations._ensureIndex({
   'users.userId': 1,
 });
-
-Organizations._ensureIndex({
-  'users.role': 1,
-});
-
-Organizations._ensureIndex({
-  'users.isRemoved': 1,
-});
-
-Organizations._ensureIndex({
-  serialNumber: 1,
-  'users.userId': 1,
-});
-
-Organizations._ensureIndex({
-  'transfer._id': 1,
-});
-
-
-// indexes for StandardsBookSections
-StandardsBookSections._ensureIndex({
-  organizationId: 1,
-});
-
-
-// indexes for StandardTypes
-StandardTypes._ensureIndex({
-  organizationId: 1,
-});
-
 
 // indexes for Standards
-Standards._ensureIndex({
-  organizationId: 1,
-});
-
-Standards._ensureIndex({
-  isDeleted: 1,
-});
-
-Standards._ensureIndex({
-  viewedBy: 1,
-});
-
 Standards._ensureIndex({
   organizationId: 1,
   isDeleted: 1,
@@ -122,42 +38,6 @@ Standards._ensureIndex({
 
 // indexes for NonConformities
 NonConformities._ensureIndex({
-  organizationId: 1,
-});
-
-NonConformities._ensureIndex({
-  isDeleted: 1,
-});
-
-NonConformities._ensureIndex({
-  viewedBy: 1,
-});
-
-NonConformities._ensureIndex({
-  title: 1,
-});
-
-NonConformities._ensureIndex({
-  sequentialId: 1,
-});
-
-NonConformities._ensureIndex({
-  departmentsIds: 1,
-});
-
-NonConformities._ensureIndex({
-  standardsIds: 1,
-});
-
-NonConformities._ensureIndex({
-  status: 1,
-});
-
-NonConformities._ensureIndex({
-  magnitude: 1,
-});
-
-NonConformities._ensureIndex({
   sequentialId: 1,
   title: 1,
 });
@@ -165,55 +45,9 @@ NonConformities._ensureIndex({
 NonConformities._ensureIndex({
   organizationId: 1,
   isDeleted: 1,
-});
-
-
-// indexes for Occurrences
-Occurrences._ensureIndex({
-  nonConformityId: 1,
 });
 
 // indexes for Risks
-Risks._ensureIndex({
-  organizationId: 1,
-});
-
-Risks._ensureIndex({
-  isDeleted: 1,
-});
-
-Risks._ensureIndex({
-  viewedBy: 1,
-});
-
-Risks._ensureIndex({
-  title: 1,
-});
-
-Risks._ensureIndex({
-  sequentialId: 1,
-});
-
-Risks._ensureIndex({
-  scores: 1,
-});
-
-Risks._ensureIndex({
-  departmentsIds: 1,
-});
-
-Risks._ensureIndex({
-  standardsIds: 1,
-});
-
-Risks._ensureIndex({
-  status: 1,
-});
-
-Risks._ensureIndex({
-  magnitude: 1,
-});
-
 Risks._ensureIndex({
   sequentialId: 1,
   title: 1,
@@ -225,30 +59,12 @@ Risks._ensureIndex({
 });
 
 // Actions indexes
-
-Actions._ensureIndex({
-  organizationId: 1,
-});
-
-Actions._ensureIndex({
-  'linkedTo.documentId': 1,
-});
-
 Actions._ensureIndex({
   organizationId: 1,
   isDeleted: 1,
 });
 
 // Work Items indexes
-
-WorkItems._ensureIndex({
-  organizationId: 1,
-});
-
-WorkItems._ensureIndex({
-  'linkedDoc._id': 1,
-});
-
 WorkItems._ensureIndex({
   organizationId: 1,
   isDeleted: 1,
@@ -275,30 +91,12 @@ WorkItems._ensureIndex({
 });
 
 // Discussions indexes
-
-Discussions._ensureIndex({
-  organizationId: 1,
-});
-
 Discussions._ensureIndex({
   linkedTo: 1,
   documentType: 1,
 });
 
 // Messages indexes
-
-Messages._ensureIndex({
-  organizationId: 1,
-});
-
-Messages._ensureIndex({
-  discussionId: 1,
-});
-
-Messages._ensureIndex({
-  createdAt: 1,
-});
-
 Messages._ensureIndex({
   discussionId: 1,
   createdAt: 1,
@@ -316,15 +114,6 @@ Messages._ensureIndex({
 });
 
 // Audit logs indexes
-
-AuditLogs._ensureIndex({
-  documentId: 1,
-});
-
-AuditLogs._ensureIndex({
-  executor: 1,
-});
-
 AuditLogs._ensureIndex({
   documentId: 1,
   executor: 1,
@@ -336,24 +125,12 @@ AuditLogs._ensureIndex({
 });
 
 // Notifications indexes
-
-Notifications._ensureIndex({
-  recepientIds: 1,
-});
-
 Notifications._ensureIndex({
   recepientIds: 1,
   createdAt: 1,
 });
 
-// Risk Types indexes
-
-RiskTypes._ensureIndex({
-  organizationId: 1,
-});
-
 // Goals indexes
-
 Goals._ensureIndex({
   organizationId: 1,
   isDeleted: 1,
@@ -372,108 +149,7 @@ Goals._ensureIndex({
   endDate: 1,
 });
 
-// Milestones indexes
-
-Milestones._ensureIndex({
-  isDeleted: 1,
-});
-
-// Channels indexes
-
-Channels._ensureIndex({
-  organizationId: 1,
-});
-
-// CustomerRelationships indexes
-
-CustomerRelationships._ensureIndex({
-  organizationId: 1,
-});
-
-// CostLines indexes
-
-CostLines._ensureIndex({
-  organizationId: 1,
-});
-
-// CustomerSegments indexes
-
-CustomerSegments._ensureIndex({
-  organizationId: 1,
-});
-
-// KeyActivities indexes
-
-KeyActivities._ensureIndex({
-  organizationId: 1,
-});
-
-// KeyPartners indexes
-
-KeyPartners._ensureIndex({
-  organizationId: 1,
-});
-
-// KeyResources indexes
-
-KeyResources._ensureIndex({
-  organizationId: 1,
-});
-
-// RevenueStreams indexes
-
-RevenueStreams._ensureIndex({
-  organizationId: 1,
-});
-
-// ValuePropositions indexes
-
-ValuePropositions._ensureIndex({
-  organizationId: 1,
-});
-
-// CanvasSettings indexes
-
-CanvasSettings._ensureIndex({
-  organizationId: 1,
-});
-
-// Customer Elements
-
-Benefits._ensureIndex({
-  organizationId: 1,
-});
-
-Benefits._ensureIndex({
-  'linkedTo.documentId': 1,
-});
-
-Features._ensureIndex({
-  organizationId: 1,
-});
-
-Features._ensureIndex({
-  'linkedTo.documentId': 1,
-});
-
-Needs._ensureIndex({
-  organizationId: 1,
-});
-
-Needs._ensureIndex({
-  'linkedTo.documentId': 1,
-});
-
-Wants._ensureIndex({
-  organizationId: 1,
-});
-
-Wants._ensureIndex({
-  'linkedTo.documentId': 1,
-});
-
 // Relations
-
 Relations._ensureIndex({
   'rel1.documentId': 1,
   'rel2.documentType': 1,
