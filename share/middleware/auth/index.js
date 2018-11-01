e8b14b2f1d43837bc2be3e8a06fdd52c91f8e8c0
Dmytro Artamonov <revels0k@gmail.com>
2018-11-01 19:36:01 +0200

    export ensureUserIsInRole middleware

--- share/middleware/auth/index.js
+++ share/middleware/auth/index.js
@@ -1,3 +1,4 @@
 export { default as checkLoggedIn } from './checkLoggedIn';
 export { default as checkOrgMembership } from './checkOrgMembership';
 export { default as ensureUserIsPlioAdmin } from './ensureUserIsPlioAdmin';
+export { default as ensureUserIsInRole } from './ensureUserIsInRole';
