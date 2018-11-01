3998a63f54aef4e6318b8ec741b687bc76eea536
Dmytro Artamonov <revels0k@gmail.com>
2018-11-01 19:27:06 +0200

    ensureUserIsInRole middleware

--- share/middleware/auth/ensureUserIsInRole.js
+++ share/middleware/auth/ensureUserIsInRole.js
@@ -3,27 +3,12 @@ import invariant from 'invariant';
 
 export default (config = () => ({})) => async (next, root, args, context) => {
   const { userId = context.userId, organizationId, role } = await config(root, args, context);
-  const { loaders: { Organization } } = context;
 
-  invariant(
-    userId && role,
-    '[ensureUserIsInRole]: "userId" and "role" are required',
-  );
+  invariant(userId && role, '"userId" and "role" are required');
 
   const isInRole = await Roles.userIsInRole(userId, role, organizationId);
 
-  if (!isInRole) {
-    let organization;
-
-    if (organizationId) {
-      organization = context.organization || (await Organization.byId.load(organizationId)) || {};
-    }
-
-    throw new Error(
-      `[ensureUserIsInRole]: User is not in role: ${Array.isArray(role) ? role.join(', ') : role}` +
-      `${organizationId ? `in the ${organization.name} organization` : ''}`,
-    );
-  }
+  invariant(isInRole, `User is not in role: ${Array.isArray(role) ? role.join(', ') : role}`);
 
   return next(root, args, context);
 };
