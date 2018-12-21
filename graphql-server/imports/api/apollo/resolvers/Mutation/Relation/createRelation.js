26b658f9cab6f018dcab67e31a0a3713d93620f1
Dmytro Artamonov <revels0k@gmail.com>
2018-12-21 13:25:06 +0200

    I1606 relations ensure organization is the same (#1694)
    * created relation mutation tests
    
    * check if both documents are of the same organization before creating
    
    * check if relation already exists before creating

--- graphql-server/imports/api/apollo/resolvers/Mutation/Relation/createRelation.js
+++ graphql-server/imports/api/apollo/resolvers/Mutation/Relation/createRelation.js
@@ -1,10 +1,12 @@
 import { applyMiddleware } from 'plio-util';
+import invariant from 'invariant';
 
 import {
   checkLoggedIn,
   flattenInput,
-  checkRelationsAccess,
+  checkDocAccess,
 } from '../../../../../share/middleware';
+import { getCollectionByDocType } from '../../../../../share/helpers';
 
 export const resolver = async (root, args, context) =>
   context.services.RelationService.insert(args, context);
@@ -12,7 +14,46 @@ export const resolver = async (root, args, context) =>
 export default applyMiddleware(
   checkLoggedIn(),
   flattenInput(),
-  checkRelationsAccess(),
+  checkDocAccess((_, { rel1: { documentId, documentType } }) => ({
+    query: { _id: documentId },
+    collection: getCollectionByDocType(documentType),
+  })),
+  async (next, root, args, context) => next(root, args, { ...context, rel1: root }),
+  checkDocAccess((_, { rel2: { documentId, documentType } }) => ({
+    query: { _id: documentId },
+    collection: getCollectionByDocType(documentType),
+  })),
+  async (next, root, args, { rel1, ...context }) => {
+    const rel2 = root;
+    const pred = (
+      rel1.organizationId && rel2.organizationId && rel1.organizationId === rel2.organizationId
+    );
+
+    invariant(pred, 'Both documents should be of the same organization');
+
+    return next(root, args, context);
+  },
+  async (next, root, args, context) => {
+    const { rel1, rel2 } = args;
+    const { collections: { Relations } } = context;
+    const query = {
+      $or: [
+        {
+          'rel1.documentId': rel1.documentId,
+          'rel2.documentId': rel2.documentId,
+        },
+        {
+          'rel1.documentId': rel2.documentId,
+          'rel2.documentId': rel1.documentId,
+        },
+      ],
+    };
+    const relation = await Relations.findOne(query);
+
+    invariant(!relation, 'Both documents are matched already');
+
+    return next(root, args, context);
+  },
   async (next, root, args, context) => {
     await next(root, args, context);
 
