259b060ef28243474184a2db086e06d3f0c2a680
Dmytro Artamonov <revels0k@gmail.com>
2018-12-29 21:39:34 +0200

    fallback to legacy nonconformity update method for meteor method (#1723)

--- app/imports/api/non-conformities/methods/update/index.js
+++ app/imports/api/non-conformities/methods/update/index.js
@@ -1,9 +1,9 @@
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
+import { IdSchema } from '../../../../share/schemas/schemas';
+import { NonConformities } from '../../../../share/collections';
+import { NonConformityService } from '../../../../share/services';
 import { CheckedMethod } from '../../../method';
-import { IdSchema } from '/imports/share/schemas/schemas';
-import { NonConformities } from '/imports/share/collections/non-conformities';
-import NonConformitiesService from '/imports/share/services/non-conformities-service';
 import UpdateSchema from './schema';
 
 export default new CheckedMethod({
@@ -14,6 +14,6 @@ export default new CheckedMethod({
   check: checker => checker(NonConformities)(() => () => true),
 
   run({ ...args }) {
-    return NonConformitiesService.update({ ...args });
+    return NonConformityService._update({ ...args });
   },
 });
