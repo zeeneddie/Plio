c029dd9d720a6c9ccf1fbf32d0fbacbc9ed4c6fa
Dmytro Artamonov <revels0k@gmail.com>
2019-01-11 13:29:34 +0200

    strip sanitizeHtml dependency from app

--- share/middleware/lesson/index.js
+++ share/middleware/lesson/index.js
@@ -1,3 +1,2 @@
 export { default as checkLessonAccess } from './checkLessonAccess';
 export { default as lessonUpdateAfterware } from './lessonUpdateAfterware';
-export { default as sanitizeNotes } from './sanitizeNotes';
