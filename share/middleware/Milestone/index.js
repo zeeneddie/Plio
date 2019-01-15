da3a7f0713eceaa70c0eef3c07ce4e57acd8d06f
Dmytro Artamonov <revels0k@gmail.com>
2019-01-15 14:06:37 +0200

    middleware: milestone -> Milestone

--- share/middleware/Milestone/index.js
+++ share/middleware/Milestone/index.js
@@ -1,6 +1,5 @@
 export { default as checkMilestoneAccess } from './checkMilestoneAccess';
 export { default as checkMilestoneLinkedDocument } from './checkMilestoneLinkedDocument';
-export {
-  default as checkMilestoneCompletionTargetDate,
-} from './checkMilestoneCompletionTargetDate';
+export { default as checkMilestoneCompletionTargetDate }
+  from './checkMilestoneCompletionTargetDate';
 export { default as milestoneUpdateAfterware } from './milestoneUpdateAfterware';
