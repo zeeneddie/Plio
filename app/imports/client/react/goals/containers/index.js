6a9fc04d288c7f1a4a3084743c1f45d837f16c49
serhiy91 <serhiy@jssolutionsdev.com>
2019-01-16 10:35:23 +0200

    delete old action components

--- app/imports/client/react/goals/containers/index.js
+++ app/imports/client/react/goals/containers/index.js
@@ -1,6 +1,5 @@
 export { default as GoalEditModalContainer } from './GoalEditModalContainer';
 export { default as GoalEditContainer } from './GoalEditContainer';
 export { default as CompletedDeletedGoalsContainer } from './CompletedDeletedGoalsContainer';
-export { default as GoalActionsSubcardContainer } from './GoalActionsSubcardContainer';
 export { default as GoalsChartContainer } from './GoalsChartContainer';
 export { default as GoalAddContainer } from './GoalAddContainer';
