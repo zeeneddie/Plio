659c524d082b06c55c0a2e68a27110376a217be9
Dmytro Artamonov <revels0k@gmail.com>
2019-02-01 06:57:23 +0200

    allow customizing goal date checker middleware arguments

--- share/middleware/Document/ensureCanUpdateStartDate.js
+++ share/middleware/Document/ensureCanUpdateStartDate.js
@@ -2,9 +2,11 @@ import invariant from 'invariant';
 
 import Errors from '../../errors';
 
-export default () => async (next, root, args, context) => {
-  const { endDate } = root;
-  const { startDate } = args;
+export default (config = () => ({})) => async (next, root, args, context) => {
+  let { startDate, endDate } = await config(root, args, context);
+
+  if (!startDate) ({ startDate } = args);
+  if (!endDate) ({ endDate } = root);
 
   invariant(
     new Date(startDate).getTime() <= new Date(endDate).getTime(),
