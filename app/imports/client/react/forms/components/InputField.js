a4eefada16ee1d606da4c3d488c71aa003b61cfa
Dmytro Artamonov <revels0k@gmail.com>
2019-02-07 15:34:24 +0200

    I1676 error with decimal percents (#1871)
    * allow float for nonconformity "financial impact" field
    
    * restrain number field if min or max props provided
    
    * added integer validation rule and combine validation util
    
    * rename standard validators and validate integer fields

--- app/imports/client/react/forms/components/InputField.js
+++ app/imports/client/react/forms/components/InputField.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Field } from 'react-final-form';
 
@@ -9,8 +10,34 @@ const renderInput = props => <InputAdapter component={FormInput} {...props} />;
 const InputField = props => (
   <Field
     component={renderInput}
+    parse={(inputValue) => {
+      if (inputValue === '') return undefined;
+
+      let value = inputValue;
+      const {
+        type,
+        min,
+        max,
+      } = props;
+
+      if (type === 'number') {
+        value = Number(value);
+
+        if (max && value > max) return max;
+
+        if (min && value < min) return min;
+      }
+
+      return value;
+    }}
     {...props}
   />
 );
+
+InputField.propTypes = {
+  type: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
+};
 
 export default InputField;
