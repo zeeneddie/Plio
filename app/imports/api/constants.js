1129d4f8b287bed775e6721a42a13e36152a2040
Dmytro Artamonov <revels0k@gmail.com>
2019-08-04 11:22:03 +0300

    bumped app version

--- app/imports/api/constants.js
+++ app/imports/api/constants.js
@@ -1,6 +1,6 @@
 import { HomeScreenTypes } from '../share/constants';
 
-export const APP_VERSION = '0.5.0';
+export const APP_VERSION = '0.5.1';
 
 export const DEFAULT_POLLING_INTERVAL_FOR_COUNTER = 5000; // 5 sec
 export const ALERT_AUTOHIDE_TIME = 1500;
